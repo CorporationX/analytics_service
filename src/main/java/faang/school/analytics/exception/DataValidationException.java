6c15dea5186b8d989edaef8120999861412df3c9
mighty-mhsl <mihusle@protonmail.com>
2023-07-05 09:47:25 +0200

    Prepared base dev branch

--- src/main/java/faang/school/analytics/exception/DataValidationException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.analytics.exception;
-
-public class DataValidationException extends BusinessException {
-    public DataValidationException(String code, String message) {
-        super(code, message);
-    }
-}
