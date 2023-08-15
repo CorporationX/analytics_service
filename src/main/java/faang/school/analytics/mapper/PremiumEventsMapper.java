6a2d31dbbd1ce0e983b1ad3bd9c253d1beb6e991
Serhii Rubets <serhii.rubets@gmail.com>
2023-08-15 17:19:05 -0500

    Fixed mapper

--- src/main/java/faang/school/analytics/mapper/PremiumEventsMapper.java
+++ src/main/java/faang/school/analytics/mapper/PremiumEventsMapper.java
@@ -4,9 +4,11 @@ import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.service.redis.events.PremiumEvent;
 import faang.school.analytics.service.redis.events.ProjectEvent;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PremiumEventsMapper {
+    @Mapping(source = "userId", target = "actorId")
     AnalyticsEvent toAnalyticsEvent(PremiumEvent premiumEvent);
 }
