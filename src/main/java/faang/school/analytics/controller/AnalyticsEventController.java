b8d2906a28453e4de800d483cdeba0b6fa493dc4
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-09-13 14:45:49 +0300

    fix after PR, add index for dates

--- src/main/java/faang/school/analytics/controller/AnalyticsEventController.java
+++ src/main/java/faang/school/analytics/controller/AnalyticsEventController.java
@@ -5,19 +5,14 @@ import faang.school.analytics.model.EventType;
 import faang.school.analytics.model.Interval;
 import faang.school.analytics.service.AnalyticsEventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,36 +27,6 @@ public class AnalyticsEventController {
                                                  @RequestParam(required = false, defaultValue = "") String eventTypeName,
                                                  @RequestParam(required = false, defaultValue = "") String interval,
                                                  @RequestParam(required = false, defaultValue = "") List<String> dates){
-        if(eventTypeName.isBlank() && eventTypeNumber == 0){
-            throw new IllegalArgumentException("Select required event type");
-        }
-        if(!eventTypeName.isBlank() && eventTypeNumber > 0){
-            throw new IllegalArgumentException("Select only one type parameter");
-        }
-        if(interval.isBlank() && dates.isEmpty()){
-            throw new IllegalArgumentException("Select required time interval");
-        }
-        if(!interval.isBlank() && dates.size() == 2){
-            throw new IllegalArgumentException("Select only interval parameter or start/end dates");
-        }
-
-        EventType eventType = null;
-        if(eventTypeName.isBlank()) {
-            eventType = EventType.of(eventTypeNumber);
-        } else if(eventTypeNumber == 0){
-            eventType = EventType.get(eventTypeName);
-        }
-
-        Interval requiredInterval = null;
-        LocalDateTime startDate = null;
-        LocalDateTime endDate = null;
-        if(dates.isEmpty()){
-            requiredInterval = Interval.get(interval);
-        }
-        if(interval.isBlank()){
-            startDate = LocalDateTime.parse(dates.get(0));
-            endDate = LocalDateTime.parse(dates.get(1));
-        }
-        return analyticsEventService.getAnalytics(idUser, eventType, requiredInterval, startDate, endDate);
+        return analyticsEventService.getAnalytics(idUser, eventTypeNumber, eventTypeName, interval, dates);
     }
 }
--- src/main/java/faang/school/analytics/service/AnalyticsEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -7,6 +7,7 @@ import faang.school.analytics.model.Interval;
 import faang.school.analytics.repository.AnalyticsEventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cglib.core.Local;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -24,8 +25,13 @@ public class AnalyticsEventService {
         analyticsEventRepository.save(analyticsEvent);
     }
 
-    public AnalyticsDto getAnalytics(Long userId, EventType eventType, Interval interval,
-                                     LocalDateTime startDate, LocalDateTime endDate) {
+    public AnalyticsDto getAnalytics(Long userId, Integer eventTypeNumber, String eventTypeName,
+                                     String intervalName, List<String> dateNames) {
+        validateForExceptions(eventTypeNumber, eventTypeName, intervalName, dateNames);
+        EventType eventType = checkEventType(eventTypeNumber, eventTypeName);
+        Interval interval = getInterval(intervalName);
+        List<LocalDateTime> dates = getDates(dateNames);
+
         Stream<AnalyticsEvent> requiredEvents = null;
         LocalDateTime today = LocalDateTime.now();
         if (interval != null) {
@@ -40,10 +46,51 @@ public class AnalyticsEventService {
                         userId, eventType, today.minusYears(1), today);
             }
         }
-        if (startDate != null && endDate != null) {
+        if (dates != null) {
+            LocalDateTime startDate = dates.get(0);
+            LocalDateTime endDate = dates.get(1);
             requiredEvents = analyticsEventRepository.findOrderedAndFilteredByTimeInterval(
                     userId, eventType, startDate, endDate);
         }
         return new AnalyticsDto(userId, requiredEvents.map(AnalyticsEvent::getActorId).toList(), eventType);
+    }
+
+    private void validateForExceptions(Integer eventTypeNumber, String eventTypeName, String interval, List<String> dates){
+        if(eventTypeName.isBlank() && eventTypeNumber == 0){
+            throw new IllegalArgumentException("Select required event type");
+        }
+        if(!eventTypeName.isBlank() && eventTypeNumber > 0){
+            throw new IllegalArgumentException("Select only one type parameter");
+        }
+        if(interval.isBlank() && dates.isEmpty()){
+            throw new IllegalArgumentException("Select required time interval");
+        }
+        if(!interval.isBlank() && dates.size() == 2){
+            throw new IllegalArgumentException("Select only interval parameter or start/end dates");
+        }
+    }
+
+    private EventType checkEventType(Integer eventTypeNumber, String eventTypeName){
+        EventType eventType = null;
+        if(eventTypeName.isBlank()) {
+            eventType = EventType.of(eventTypeNumber);
+        } else if(eventTypeNumber == 0){
+            eventType = EventType.get(eventTypeName);
+        }
+        return eventType;
+    }
+
+    private Interval getInterval(String interval){
+        if(interval != null) {
+            return Interval.get(interval);
+        }
+        return null;
+    }
+
+    private List<LocalDateTime> getDates(List<String> dates){
+        if(dates != null) {
+            return List.of(LocalDateTime.parse(dates.get(0)), LocalDateTime.parse(dates.get(1)));
+        }
+        return null;
     }
 }
