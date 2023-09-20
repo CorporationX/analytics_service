07ae962ba084218fefaf6d8440ca347a47ae9755
Abramov Nikita <mr.abramovn@gmail.com>
2023-09-20 11:54:09 +0300

    fix application.yaml after review

--- src/main/java/faang/school/analytics/client/UserServiceClient.java
+++
@@ -1,8 +0,0 @@
-package faang.school.analytics.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
-public interface UserServiceClient {
-
-}
