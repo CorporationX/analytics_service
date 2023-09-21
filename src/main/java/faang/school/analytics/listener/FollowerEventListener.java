c4e7c0144bda9748e12fd283a211a14ce306c369
Angie <angie@>
2023-09-21 20:01:04 +0300

    fixed

--- src/main/java/faang/school/analytics/listener/FollowerEventListener.java
+++ src/main/java/faang/school/analytics/listener/FollowerEventListener.java
@@ -7,7 +7,9 @@ import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.model.EventType;
 import faang.school.analytics.service.AnalyticsEventService;
 import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FollowerEventListener extends AbstractEventListener<FollowerEventDto>{
 
     public FollowerEventListener(ObjectMapper objectMapper,
