aca6beacd0f172e767d86c19c8402b4444bea8aa
MalinaInk <nataleshen.ko@yandex.ru>
2023-10-05 17:12:10 +0700

    fixed mapping(zoneDateTime

--- src/main/java/faang/school/analytics/mapper/FollowerEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/FollowerEventMapper.java
@@ -3,22 +3,33 @@ package faang.school.analytics.mapper;
 import faang.school.analytics.dto.AnalyticsEventDto;
 import faang.school.analytics.dto.FollowerEventDto;
 import faang.school.analytics.model.AnalyticsEvent;
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface FollowerEventMapper {
     @Mapping(target = "actorId", source = "followerId")
     @Mapping(target = "receiverId", source = "followeeId")
-    @Mapping(target = "receivedAt", source = "subscriptionTime")
+    @Mapping(source = "receivedAt", target = "subscriptionTime", qualifiedByName = "zoneToLocalDateTime")
     AnalyticsEvent toModel(FollowerEventDto eventDto);
 
     @Mapping(target = "actorId", source = "followerId")
     @Mapping(target = "receiverId", source = "followeeId")
-    @Mapping(target = "receivedAt", source = "subscriptionTime")
+    @Mapping(source = "receivedAt", target = "subscriptionTime", qualifiedByName = "localToZoneDateTime")
     AnalyticsEventDto toDto(FollowerEventDto eventDto);
 
-}
+        @Named("zoneToLocalDateTime")
+        default LocalDateTime zoneToLocalDateTime(ZonedDateTime zonedDateTime){
+            return zonedDateTime.toLocalDateTime();
+        }
+
+        @Named("localToZoneDateTime")
+        default ZonedDateTime localToZoneDateTime(LocalDateTime localDateTime){
+            ZoneId zoneId = ZoneId.of("UTC");
+            return localDateTime.atZone(zoneId);
+        }
+    }
