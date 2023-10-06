1eb6bbe9b2269b5bc7e9fa239fa8c1cb1fb9cc7f
alexandr <matcnbev2001@mail.ru>
2023-10-06 18:53:28 -0400

    add annotations

--- src/main/java/faang/school/analytics/listener/LikePostEventListener.java
+++ src/main/java/faang/school/analytics/listener/LikePostEventListener.java
@@ -6,9 +6,14 @@ import faang.school.analytics.mapper.AnalyticsEventMapper;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.model.EventType;
 import faang.school.analytics.service.AnalyticsEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LikePostEventListener extends AbstractEventListener<LikePostEventDto> {
+
+    @Autowired
     public LikePostEventListener(ObjectMapper objectMapper, AnalyticsEventMapper analyticsMapper, AnalyticsEventService analyticsEventService) {
         super(objectMapper, analyticsMapper, analyticsEventService);
     }
