15a7de778eb132f4525292d1aa1e2a22c91b64de
Azat <azat.takhautdinov@gmail.com>
2023-10-15 01:08:49 +0500

    убраны импорты

--- src/main/java/faang/school/analytics/messaging/RecommendationReceivedEventListener.java
+++ src/main/java/faang/school/analytics/messaging/RecommendationReceivedEventListener.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
