3a1fcc83f83f54ce6f97f82d27e46b2759572e48
Паша <petrov.pashapet@yandex.ru>
2023-10-18 04:12:59 +0300

    Анализ посещаемости профиля проекта

--- src/main/java/faang/school/analytics/dto/ProjectViewDto.java
+++ src/main/java/faang/school/analytics/dto/ProjectViewDto.java
@@ -2,10 +2,12 @@ package faang.school.analytics.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ProjectViewDto {
     private Long projectId;
--- src/main/java/faang/school/analytics/listener/ProjectViewEventListener.java
+++ src/main/java/faang/school/analytics/listener/ProjectViewEventListener.java
@@ -1,6 +1,5 @@
 package faang.school.analytics.listener;
 
-import faang.school.analytics.dto.CommentEventDto;
 import faang.school.analytics.dto.EventDto;
 import faang.school.analytics.dto.ProjectViewDto;
 import faang.school.analytics.mapper.JsonObjectMapper;
@@ -11,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
+
+import static faang.school.analytics.model.EventType.PROJECT_VIEW;
 
 @Component
 @RequiredArgsConstructor
@@ -24,6 +25,7 @@ public class ProjectViewEventListener implements MessageListener {
     public void onMessage(Message message, byte[] pattern) {
         ProjectViewDto projectViewDto = jsonObjectMapper.readValue(message.getBody(), ProjectViewDto.class);
         EventDto eventDto = eventMapper.toEventDto(projectViewDto);
+        eventDto.setEventType(PROJECT_VIEW);
         analyticsEventService.saveEvent(eventDto);
     }
 }
