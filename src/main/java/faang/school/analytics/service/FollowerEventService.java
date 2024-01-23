4bb91f749a2f6baa4d15f17f190ecc0f7ce3ddf8
Kamil Gasanov <gasanovkamil516@gmail.com>
2024-01-23 15:46:23 +0300

    added mappers

--- src/main/java/faang/school/analytics/service/FollowerEventService.java
+++ src/main/java/faang/school/analytics/service/FollowerEventService.java
@@ -1,25 +1,19 @@
 package faang.school.analytics.service;
 
 import faang.school.analytics.dto.FollowerEvent;
+import faang.school.analytics.mapper.FollowerEventMapper;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.repository.AnalyticsEventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import static faang.school.analytics.model.EventType.FOLLOWER;
-
 @Component
 @RequiredArgsConstructor
 public class FollowerEventService {
     private final AnalyticsEventRepository analyticsEventRepository;
-
+    private final FollowerEventMapper followerEventMapper;
     public void save(FollowerEvent followerEvent) {
-        AnalyticsEvent model = AnalyticsEvent.builder()
-                .receiverId(followerEvent.getFolloweeId())
-                .actorId(FOLLOWER.ordinal())
-                .eventType(FOLLOWER)
-                .receivedAt(followerEvent.getTimestamp())
-                .build();
-        analyticsEventRepository.save(model);
+        AnalyticsEvent analyticsEvent = followerEventMapper.toAnalyticsEvent(followerEvent);
+        analyticsEventRepository.save(analyticsEvent);
     }
 }
--- src/main/java/faang/school/analytics/service/PremiumEventService.java
+++ src/main/java/faang/school/analytics/service/PremiumEventService.java
@@ -1,36 +1,20 @@
 package faang.school.analytics.service;
 
 import faang.school.analytics.dto.premium.PremiumEvent;
-import faang.school.analytics.dto.premium.PremiumPeriod;
+import faang.school.analytics.mapper.PremiumEventMapper;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.repository.AnalyticsEventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import static faang.school.analytics.model.EventType.*;
-
 @Component
 @RequiredArgsConstructor
 public class PremiumEventService {
     private final AnalyticsEventRepository analyticsEventRepository;
+    private final PremiumEventMapper premiumEventMapper;
 
     public void save(PremiumEvent premiumEvent) {
-        AnalyticsEvent model = AnalyticsEvent.builder()
-                .receiverId(premiumEvent.getUserId())
-                .receivedAt(premiumEvent.getTimestamp())
-                .build();
-
-        if (premiumEvent.getPremiumPeriod() == PremiumPeriod.ONE_MONTH){
-            model.setEventType(PREMIUM_ONE_MONTH);
-            model.setActorId(PREMIUM_ONE_MONTH.ordinal());
-        } else if (premiumEvent.getPremiumPeriod() == PremiumPeriod.THREE_MONTHS) {
-            model.setEventType(PREMIUM_THREE_MONTHS);
-            model.setActorId(PREMIUM_THREE_MONTHS.ordinal());
-        } else {
-            model.setEventType(PREMIUM_ONE_YEAR);
-            model.setActorId(PREMIUM_ONE_YEAR.ordinal());
-        }
-
-        analyticsEventRepository.save(model);
+        AnalyticsEvent analyticsEvent = premiumEventMapper.toAnalyticsEvent(premiumEvent);
+        analyticsEventRepository.save(analyticsEvent);
     }
 }
