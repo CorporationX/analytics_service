5102519cf3551d21bb6835800e9e34e8ea2a7249
Ackerman995 <rauanture@gmail.com>
2024-02-22 21:12:28 +0600

    create AbstractEventListener and AnalyticsEventService. Topic is RecommendationEvent.

---
+++ src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
@@ -0,0 +1,16 @@
+package faang.school.analytics.mapper;
+
+import faang.school.analytics.dto.RecommendationEvent;
+import faang.school.analytics.model.AnalyticsEvent;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "Spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface AnalyticsEventMapper {
+    @Mapping(source = "recommendationId", target = "id")
+    @Mapping(source = "authorId", target = "actorId")
+    @Mapping(source = "createdAt", target = "receivedAt")
+    AnalyticsEvent toEntity(RecommendationEvent recommendationEvent);
+
+}
---
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -0,0 +1,25 @@
+package faang.school.analytics.service;
+
+import faang.school.analytics.dto.RecommendationEvent;
+import faang.school.analytics.mapper.AnalyticsEventMapper;
+import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.model.EventType;
+import faang.school.analytics.repository.AnalyticsEventRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AnalyticsEventService {
+    private final AnalyticsEventMapper analyticsEventMapper;
+    private final AnalyticsEventRepository analyticsEventRepository;
+
+    public void saveRecommendationEvent(RecommendationEvent recommendationEvent) {
+        AnalyticsEvent analyticsEvent = analyticsEventMapper.toEntity(recommendationEvent);
+        analyticsEvent.setEventType(EventType.RECOMMENDATION_RECEIVED);
+        analyticsEventRepository.save(analyticsEvent);
+        log.info(recommendationEvent + " is saved to DB");
+    }
+}
