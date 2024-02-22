d5363dd41060e4a363c033cc72dbff6d73b97a52
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-02-22 11:23:50 +0300

    тестики

--- src/main/java/faang/school/analytics/mapper/GoalCompletedEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/GoalCompletedEventMapper.java
@@ -4,7 +4,6 @@ import faang.school.analytics.dto.GoalCompletedEvent;
 import faang.school.analytics.model.AnalyticsEvent;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -12,6 +11,6 @@ public interface GoalCompletedEventMapper {
     @Mapping(target = "eventType", expression = "java(faang.school.analytics.model.EventType.GOAL_COMPLETED)")
     @Mapping(target = "receivedAt", source = "timestamp")
     @Mapping(target = "receiverId", source = "userId")
-    //@Mapping(target = "actorId", source = "goalId") under the question
+    @Mapping(target = "actorId", source = "goalId")
     AnalyticsEvent toAnalyticsEvent(GoalCompletedEvent goalCompletedEvent);
 }
