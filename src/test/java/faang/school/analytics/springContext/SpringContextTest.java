1eff1aa9560580d83830978ce4eaeb6e37e7bf6f
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-03-03 11:21:37 +0300

    тест поднятия спринг контекста

---
+++ src/test/java/faang/school/analytics/springContext/SpringContextTest.java
@@ -0,0 +1,12 @@
+package faang.school.analytics.springContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class SpringContextTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
