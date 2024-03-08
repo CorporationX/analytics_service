f64a627cc4bdffbfe3d69554e24029453e0a8088
phoen1x <106927036+vladmeniailov@users.noreply.github.com>
2024-03-08 18:58:42 +0300

    Create general service and mapper.

--- src/main/java/faang/school/analytics/listener/PremiumBoughtEventListener.java
+++ src/main/java/faang/school/analytics/listener/PremiumBoughtEventListener.java
@@ -2,24 +2,27 @@ package faang.school.analytics.listener;
 
 
 import faang.school.analytics.dto.PremiumBoughtEvent;
-import faang.school.analytics.service.AnalyticsEventService;
+import faang.school.analytics.model.AnalyticsEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class PremiumBoughtEventListener extends AbstractEventListener<PremiumBoughtEvent> {
-    @Autowired
-    private AnalyticsEventService<PremiumBoughtEvent> premiumBoughtService;
 
     public PremiumBoughtEventListener() {
         super(PremiumBoughtEvent.class);
     }
 
     @Override
+    protected AnalyticsEvent mapEvent(PremiumBoughtEvent event) {
+        return eventMapper.toAnalyticsEvent(event);
+    }
+
+    @Override
     public void saveEvent(PremiumBoughtEvent event) {
-        premiumBoughtService.saveEvent(event);
+        AnalyticsEvent analyticsEvent = mapEvent(event);
+        analyticsEventService.saveEvent(analyticsEvent);
     }
 
 }
--- src/main/java/faang/school/analytics/service/AnalyticsEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -1,6 +1,5 @@
 package faang.school.analytics.service;
 
-import faang.school.analytics.mapper.AnalyticsEventMapper;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.model.EventType;
 import faang.school.analytics.repository.AnalyticsEventRepository;
@@ -8,20 +7,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.stream.Stream;
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class AnalyticsEventService<T> {
+public class AnalyticsEventService {
 
     private final AnalyticsEventRepository analyticsEventRepository;
-    private final AnalyticsEventMapper<T> analyticsEventMapper;
 
-    public void saveEvent(T event) {
-        AnalyticsEvent analyticsEvent = analyticsEventMapper.toAnalyticsEvent(event);
-        analyticsEventRepository.save(analyticsEvent);
-        log.info("Event successful saved");
+    public void saveEvent(AnalyticsEvent event) {
+        analyticsEventRepository.save(event);
+        log.info("Event successful saved {}", event);
     }
 
     public void deleteEvent(long id) {
@@ -29,8 +26,8 @@ public class AnalyticsEventService<T> {
         log.info("Event successful deleted");
     }
 
-    public Stream<AnalyticsEvent> getAnalytics(long id, EventType type) {
-        return analyticsEventRepository.findByReceiverIdAndEventType(id, type);
+    public List<AnalyticsEvent> getAnalytics(long id, EventType type) {
+        return analyticsEventRepository.findByReceiverIdAndEventType(id, type).toList();
     }
 
 }
