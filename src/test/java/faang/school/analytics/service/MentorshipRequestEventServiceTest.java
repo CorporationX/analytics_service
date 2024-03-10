809e30a10f84ede1e5d5ab81389dd670820090fc
Ilia Chuvatkin <ilyachuvatkin@gmail.com>
2024-03-10 23:55:17 +0300

    added test

---
+++ src/test/java/faang/school/analytics/service/MentorshipRequestEventServiceTest.java
@@ -0,0 +1,62 @@
+package faang.school.analytics.service;
+
+import faang.school.analytics.dto.MentorshipRequestedEventDto;
+import faang.school.analytics.mapper.MentorshipRequestedMapperImpl;
+import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.model.EventType;
+import faang.school.analytics.repository.AnalyticsEventRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MentorshipRequestEventServiceTest {
+    @Mock
+    private AnalyticsEventRepository analyticsEventRepository;
+    @Spy
+    private MentorshipRequestedMapperImpl mentorshipRequestedMapper;
+    @InjectMocks
+    private AnalyticsEventService<MentorshipRequestedEventDto> analyticsEventService;
+
+    @Test
+    void successSaveEvent() {
+        MentorshipRequestedEventDto eventDto = MentorshipRequestedEventDto.builder()
+                .requesterId(1L)
+                .receiverId(2L)
+                .receivedAt(LocalDateTime.now())
+                .build();
+        analyticsEventService.saveEvent(eventDto);
+        AnalyticsEvent event = mentorshipRequestedMapper.toAnalyticsEvent(eventDto);
+        verify(analyticsEventRepository, times(1)).save(event);
+    }
+
+    @Test
+    void getAnalytics() {
+        AnalyticsEvent analyticsEvent = AnalyticsEvent.builder()
+                .id(1L)
+                .actorId(1L)
+                .eventType(EventType.MENTORSHIP_REQUEST)
+                .receiverId(1L)
+                .build();
+        Stream<AnalyticsEvent> analyticsEventStream = Stream.of(analyticsEvent);
+        long id = analyticsEvent.getId();
+        EventType eventType = analyticsEvent.getEventType();
+
+        when(analyticsEventRepository.findByReceiverIdAndEventType(id, eventType)).thenReturn(analyticsEventStream);
+        Stream<AnalyticsEvent> actualAnalyticsEvents = analyticsEventService.getAnalytics(id, eventType);
+
+        verify(analyticsEventRepository, times(1)).findByReceiverIdAndEventType(id, eventType);
+        assertEquals(analyticsEventStream, actualAnalyticsEvents);
+    }
+}
