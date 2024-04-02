66c04aaddf1d9584ef6f2b59f9d3a589c3c9b3f3
Edisolt Khatsiev <Edisolt Khatsiev>
2024-04-03 00:34:52 +0300

    mapper fixed

--- src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
@@ -10,6 +10,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AnalyticsEventMapper {
 
