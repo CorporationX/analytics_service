efb741a386682067787cb34e015e8164d45f2bc6
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-10 14:39:08 +0300

    Настроиваю редис

--- src/main/java/faang/school/analytics/listener/ProjectViewEventListener.java
+++ src/main/java/faang/school/analytics/listener/ProjectViewEventListener.java
@@ -3,6 +3,7 @@ package faang.school.analytics.listener;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.analytics.dto.AnalyticsEventDto;
+import faang.school.analytics.mapper.AnalyticsEventMapper;
 import faang.school.analytics.model.EventType;
 import faang.school.analytics.service.AnalyticsEventService;
 import lombok.RequiredArgsConstructor;
@@ -13,40 +14,39 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class ProjectViewEventListener implements MessageListener {
-    private AnalyticsEventService analyticsEventService;
-
     @Async
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        Map<String, Object> data;
+        Map<String, String> data;
         try {
             String jsonMessage = new String((byte[]) message.getBody());
-            data = new ObjectMapper().readValue(jsonMessage, Map.class);
+             data = new ObjectMapper().readValue(jsonMessage, Map.class);
         } catch (JsonProcessingException e) {
             log.error("Error parsing JSON message: {}", e.getMessage());
             return;
         }
 
         String userId = (String) data.get("userId");
-        Long projectId = (Long) data.get("projectId");
-        LocalDateTime timestamp = LocalDateTime.parse((String) data.get("timestamp"));
+        String projectId = (String) data.get("projectId");
+        String timestamp = data.get("timestamp");
 
         log.info("Received ProjectViewEvent: userId={}, projectId={}, timestamp={}", userId, projectId, timestamp);
 
         try {
             AnalyticsEventDto eventDto = new AnalyticsEventDto();
-            eventDto.setReceiverId(projectId);
+            eventDto.setReceiverId(Long.parseLong(projectId));
             eventDto.setActorId(Long.parseLong(userId));
             eventDto.setEventType(EventType.PROJECT_VIEW);
-            eventDto.setReceivedAt(timestamp);
+            eventDto.setReceivedAt(LocalDateTime.parse(timestamp));
 
-            analyticsEventService.saveEvent(eventDto);
+            AnalyticsEventService analyticsEventService
 
         } catch (Exception e) {
             log.error("Error processing ProjectViewEvent: {}", e.getMessage());
