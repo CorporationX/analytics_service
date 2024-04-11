205d0d175b8b87c63a4213ef681ba5eda96fb3c4
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-11 22:42:36 +0300

    CR

--- src/main/java/faang/school/analytics/config/context/redis/RedisConfig.java
+++ src/main/java/faang/school/analytics/config/context/redis/RedisConfig.java
@@ -3,55 +3,60 @@ package faang.school.analytics.config.context.redis;
 import faang.school.analytics.listener.ProjectViewEventListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
+
     private final ProjectViewEventListener projectViewEventListener;
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.channels.project_view_channel.name}")
+    private String channel;
+
     @Bean
-    LettuceConnectionFactory lettuceConnectionFactory(){
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setPort(6379);
-        configuration.setHostName("localhost");
-        return new LettuceConnectionFactory(configuration);
+    JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(configuration);
     }
 
     @Bean
-    RedisTemplate<String, Object> redisTemplate(){
-        final RedisTemplate<String,Object> template = new RedisTemplate<String,Object>();
-        template.setConnectionFactory(lettuceConnectionFactory());
+    RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(redisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        template.setValueSerializer(new StringRedisSerializer());
         return template;
     }
 
     @Bean
-    MessageListenerAdapter messageListener(){
+    MessageListenerAdapter messageListener() {
         return new MessageListenerAdapter(projectViewEventListener);
     }
+
     @Bean
-    RedisMessageListenerContainer redisContainer(){
+    RedisMessageListenerContainer redisContainer() {
         final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(lettuceConnectionFactory());
-        container.addMessageListener( messageListener(), topic());
+        container.setConnectionFactory(redisConnectionFactory());
+        container.addMessageListener(messageListener(), topic());
         return container;
     }
 
     @Bean
     public ChannelTopic topic() {
-        return new ChannelTopic("view-project");
+        return new ChannelTopic(channel);
     }
-
 }
