6d9d49042d55495a76cec55d64c7dcf2a7c79d11
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-17 17:40:16 +0300

    осознал ошибки , возможно не все

--- src/main/java/faang/school/analytics/listener/ProjectViewEventListener.java
+++ src/main/java/faang/school/analytics/listener/ProjectViewEventListener.java
@@ -3,6 +3,8 @@ package faang.school.analytics.listener;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.analytics.dto.AnalyticsEventDto;
+import faang.school.analytics.dto.ProjectViewEventDto;
+import faang.school.analytics.mapper.AnalyticsEventMapper;
 import faang.school.analytics.model.EventType;
 import faang.school.analytics.service.AnalyticsEventService;
 import lombok.RequiredArgsConstructor;
@@ -13,40 +15,26 @@ import org.springframework.data.redis.serializer.SerializationException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class ProjectViewEventListener implements MessageListener {
     private final AnalyticsEventService analyticsEventService;
+    private final AnalyticsEventMapper analyticsEventMapper;
 
     @Async
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        log.info("InviteEventListener has received a new message from Redis")
-        Map<String, String> data;
+        log.info("InviteEventListener has received a new message from Redis");
         try {
             String jsonMessage = new String((byte[]) message.getBody());
-            data = new ObjectMapper().readValue(jsonMessage, Map.class);
+            ProjectViewEventDto projectViewEventDto = new ObjectMapper().readValue(jsonMessage, ProjectViewEventDto.class);
+            AnalyticsEventDto eventDto = analyticsEventMapper.toAnalyticsEventDto(projectViewEventDto);
+            eventDto.setEventType(EventType.PROJECT_VIEW);
+            analyticsEventService.saveEvent(eventDto);
         } catch (JsonProcessingException e) {
             log.info("Error parsing JSON message");
             throw new SerializationException("Error parsing JSON message");
         }
-
-        String userId = (String) data.get("userId");
-        String projectId = (String) data.get("projectId");
-        String timestamp = data.get("timestamp");
-
-        log.info("Received ProjectViewEvent: userId={}, projectId={}, timestamp={}", userId, projectId, timestamp);
-
-
-        AnalyticsEventDto eventDto = new AnalyticsEventDto();
-        eventDto.setReceiverId(Long.parseLong(projectId));
-        eventDto.setActorId(Long.parseLong(userId));
-        eventDto.setEventType(EventType.PROJECT_VIEW);
-        eventDto.setReceivedAt(LocalDateTime.parse(timestamp));
-        analyticsEventService.saveEvent(eventDto);
     }
 }
--- src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
@@ -1,11 +1,20 @@
 package faang.school.analytics.mapper;
 
 import faang.school.analytics.dto.AnalyticsEventDto;
+import faang.school.analytics.dto.ProjectViewEventDto;
 import faang.school.analytics.model.AnalyticsEvent;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AnalyticsEventMapper {
+
     AnalyticsEvent toEntity(AnalyticsEventDto analyticsEventDto);
+
     AnalyticsEventDto toDto(AnalyticsEvent analyticsEvent);
+
+    @Mapping(target = "receiverId", source = "projectId")
+    @Mapping(target = "actorId", source = "userId")
+    @Mapping(target = "receivedAt", source = "timestamp")
+    AnalyticsEventDto toAnalyticsEventDto(ProjectViewEventDto projectViewEventDto);
 }
