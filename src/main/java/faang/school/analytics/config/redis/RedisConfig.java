29ae3193635cbfecf63552b235588dc25d9ce46d
Tem <tipokruto014@gmail.com>
2024-06-03 12:11:48 +0300

    initial

---
+++ src/main/java/faang/school/analytics/config/redis/RedisConfig.java
@@ -0,0 +1,55 @@
+package faang.school.analytics.config.redis;
+
+import faang.school.analytics.consumer.CompletedGoalConsumer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    private final CompletedGoalConsumer completedGoalConsumer;
+    private final CompletedGoalRedisConfig completedGoalRedisConfig;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        System.out.println(port);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        MessageListenerAdapter messageListenerAdapter = new MessageListenerAdapter(completedGoalConsumer);
+        container.addMessageListener(messageListenerAdapter, completedGoalRedisConfig.completedGoalTopic());
+
+        return container;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
---
+++ src/main/java/faang/school/analytics/consumer/CompletedGoalConsumer.java
@@ -0,0 +1,23 @@
+package faang.school.analytics.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CompletedGoalConsumer implements MessageListener {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String channel = new String(message.getChannel());
+        String body = new String(message.getBody());
+        log.info("Received completed goal message from channel: {}, body: {}", channel, body);
+    }
+}
