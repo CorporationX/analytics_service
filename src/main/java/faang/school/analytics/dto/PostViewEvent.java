6c71cc6899f991e533b75c3fc4be5d461e22baaa
romantarelicev <romuhut@mail.ru>
2024-06-10 14:23:28 +0300

    some changes

--- src/main/java/faang/school/analytics/dto/PostViewEvent.java
+++ src/main/java/faang/school/analytics/dto/PostViewEvent.java
@@ -1,14 +1,19 @@
 package faang.school.analytics.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostViewEvent {
-    private long receiverId;
-    private long actorId;
-    private LocalDateTime receivedAt;
+    private long postId;
+    private long authorId;
+    private long userId;
+    private LocalDateTime viewedAt;
 }
