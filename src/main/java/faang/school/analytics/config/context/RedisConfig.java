1011151ae9887b918717e3dc570da7722c1e7812
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-06-12 00:01:26 +0300

    redis config

---
+++ src/main/java/faang/school/analytics/config/context/RedisConfig.java
@@ -0,0 +1,52 @@
+package faang.school.analytics.config.context;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${data.redis.channel.post-view-channel}")
+    private String postViewChannelName;
+
+    @Value("${data.redis.port}")
+    private int redisPort;
+
+    @Value("${data.redis.host}")
+    private String redisHost;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+
+        return container;
+    }
+
+    @Bean
+    public ChannelTopic postViewTopic() {
+        return new ChannelTopic(postViewChannelName);
+    }
+}
