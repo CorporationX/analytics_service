6466d7d286846e0cafc5c01513d49b213e751960
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-06-13 15:32:41 +0300

    create tests

---
+++ src/test/java/faang/school/analytics/listener/PostViewEventListenerTest.java
@@ -0,0 +1,83 @@
+package faang.school.analytics.listener;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.analytics.dto.PostViewEvent;
+import faang.school.analytics.listner.PostViewEventListener;
+import faang.school.analytics.mapper.AnalyticsEventMapper;
+import faang.school.analytics.mapper.AnalyticsEventMapperImpl;
+import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.model.EventType;
+import faang.school.analytics.service.AnalyticsEventService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.connection.Message;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyByte;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
+public class PostViewEventListenerTest {
+
+    @InjectMocks
+    private PostViewEventListener postViewEventListener;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private AnalyticsEventService analyticsEventService;
+
+    @Mock
+    private AnalyticsEventMapperImpl analyticsEventMapper;
+
+    @Mock
+    private AnalyticsEvent analyticsEvent;
+
+    @Mock
+    private Message message;
+
+    @Test
+    void testHandlePostViewEventThenSave() throws IOException {
+        String json = "{\"key\":\"value\"}";
+        PostViewEvent event = new PostViewEvent();
+        byte[] body = json.getBytes();
+
+        when(message.getBody()).thenReturn(body);
+        when(objectMapper.readValue(body, PostViewEvent.class)).thenReturn(event);
+        when(analyticsEventMapper.toEntity(event)).thenReturn(analyticsEvent);
+
+        postViewEventListener.onMessage(message, null);
+
+        verify(message).getBody();
+        verify(objectMapper).readValue(body, PostViewEvent.class);
+        verify(analyticsEventMapper).toEntity(event);
+        verify(analyticsEvent).setEventType(EventType.POST_VIEW);
+        verify(analyticsEventService).saveEvent(analyticsEvent);
+    }
+
+    @Test
+    void testHandleThenThrowException() throws IOException {
+        String json = "{\"key\":\"value\"}";
+        byte[] body = json.getBytes();
+
+        when(message.getBody()).thenReturn(body);
+        when(objectMapper.readValue(body, PostViewEvent.class)).thenThrow(new IOException("Mapping error"));
+
+        postViewEventListener.onMessage(message, null);
+
+        verify(message).getBody();
+        verify(objectMapper).readValue(body, PostViewEvent.class);
+        verify(analyticsEventMapper, never()).toEntity(any());
+        verify(analyticsEventService, never()).saveEvent(any());
+    }
+
+}
