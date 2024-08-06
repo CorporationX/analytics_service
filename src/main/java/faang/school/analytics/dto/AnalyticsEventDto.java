90b4864dedd0b177dc1ed14167443a661b755621
Kirill Maltsev <opp.2211@gmail.com>
2024-08-07 01:58:07 +0500

    dto fields

--- src/main/java/faang/school/analytics/dto/AnalyticsEventDto.java
+++ src/main/java/faang/school/analytics/dto/AnalyticsEventDto.java
@@ -1,10 +1,24 @@
 package faang.school.analytics.dto;
 
+import faang.school.analytics.model.EventType;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class AnalyticsEventDto {
+    long id;
 
+    long receiverId;
+
+    long actorId;
+
+    EventType eventType;
+
+    LocalDateTime receivedAt;
 }
