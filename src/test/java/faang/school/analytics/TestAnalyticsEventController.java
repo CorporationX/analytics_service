80b79da590d5842a68b75f6980d73e6b5c2ac137
Max Shundrick <mas9l175@gmail.com>
2024-08-11 00:03:16 +0300

    s

--- src/test/java/faang/school/analytics/TestAnalyticsEventController.java
+++ src/test/java/faang/school/analytics/TestAnalyticsEventController.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
