5c9074589493e677c6f5e0c4e3719838001bcb99
OlgaK888 <olgakurilovi4@gmail.com>
2024-08-10 18:27:49 +0300

    Create method for mapping CommentEvent to AnalyticsEvent

--- src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
@@ -2,11 +2,17 @@ package faang.school.analytics.mapper;
 
 import faang.school.analytics.dto.AnalyticsEventDto;
 import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.model.CommentEvent;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AnalyticsEventMapper {
 
     AnalyticsEventDto toDto(AnalyticsEvent analyticsEvent);
+
+    @Mapping(source = "commentId", target = "receiverId")
+    @Mapping(source = "authorId", target = "actorId")
+    AnalyticsEvent commentEventToAnalyticsEvent(CommentEvent commentEvent);
 }
