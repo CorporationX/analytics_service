698c1f63301b5d6f891efe7047f0780d7a1ad122
pavlick2745 <Pa4aPV@yandex.ru>
2024-08-11 20:43:44 +0700

    BJS2-15664 - "Анализ просмотров постов"
    1. реализовал потребителя события просмотров поста, происвожу мапинг полученного событие в сущности аналитики и сохроняю в бд

---
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -0,0 +1,20 @@
+package faang.school.analytics.service;
+
+import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.repository.AnalyticsEventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AnalyticsEventService {
+
+    private final AnalyticsEventRepository analyticsEventRepository;
+
+    @Transactional
+    public void saveEvent(AnalyticsEvent analyticsEvent) {
+        analyticsEventRepository.save(analyticsEvent);
+    }
+}
