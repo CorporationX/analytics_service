53ec929f88e273d243da90af1b3765be12e96dbf
Kirill Maltsev <opp.2211@gmail.com>
2024-08-14 08:43:53 +0500

    feat: handle Redis like_topic message

--- src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
@@ -1,12 +1,20 @@
 package faang.school.analytics.mapper;
 
 import faang.school.analytics.dto.AnalyticsEventDto;
+import faang.school.analytics.event.PostLikeEvent;
 import faang.school.analytics.model.AnalyticsEvent;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AnalyticsEventMapper {
 
     AnalyticsEventDto toDto(AnalyticsEvent analyticsEvent);
+
+
+    @Mapping(source = "postId", target = "receiverId")
+    @Mapping(source = "actorId", target = "actorId")
+    @Mapping(source = "createdAt", target = "receivedAt")
+    AnalyticsEvent toAnalyticsEvent(PostLikeEvent postLikeEvent);
 
 }
