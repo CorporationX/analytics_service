7469bc7dbe5fd94d84bc6f0033651eeef3fb7a21
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-08-17 02:24:26 +0500

    BJS2-15668 - Запрос аналитики
    + Добавил transactional

--- src/main/java/faang/school/analytics/service/AnalyticsEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -4,12 +4,14 @@ import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.repository.AnalyticsEventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AnalyticsEventService {
     private final AnalyticsEventRepository analyticsEventRepository;
 
+    @Transactional
     public void saveAnalyticsEvent(AnalyticsEvent analyticsEvent) {
         analyticsEventRepository.save(analyticsEvent);
     }
