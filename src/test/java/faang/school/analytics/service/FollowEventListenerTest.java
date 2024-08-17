5198b8d7033ca20e3b0150e8e1460d1f525957f1
Andrei Kuzmin <Andreytko@mail.ru>
2024-08-17 23:49:47 +0300

    написал тест для FollowerEventListener

---
+++ src/test/java/faang/school/analytics/service/FollowEventListenerTest.java
@@ -0,0 +1,67 @@
+package faang.school.analytics.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.analytics.client.UserServiceClient;
+import faang.school.analytics.dto.FollowerEvent;
+import faang.school.analytics.dto.UserDto;
+import faang.school.analytics.mapper.AnalyticsEventMapper;
+import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.model.EventType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.connection.Message;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@ExtendWith(MockitoExtension.class)
+public class FollowEventListenerTest {
+    @Mock
+    private AnalyticsEventMapper analyticsEventMapper;
+    @Mock
+    private UserServiceClient userServiceClient;
+    @Mock
+    private ObjectMapper objectMapper;
+    @Mock
+    private AnalyticsEventService analyticsEventService;
+    @Mock
+    private Message message;
+    @InjectMocks
+    private FollowerEventListener followEventListener;
+
+    private FollowerEvent followerEvent;
+    private AnalyticsEvent analyticsEvent;
+    private UserDto follower;
+    private UserDto followee;
+    private byte[] pattern;
+
+    @BeforeEach
+    public void setUp() {
+        followerEvent = FollowerEvent.builder().followerId(1L).followeeId(2L).
+                followTime(LocalDateTime.of(2024,8, 17, 0, 0)).build();
+        analyticsEvent = AnalyticsEvent.builder().id(0L).actorId(1L).receiverId(2L).eventType(EventType.FOLLOWER)
+                .receivedAt(LocalDateTime.of(2024,8, 17, 0, 0)).build();
+        follower = new UserDto(1L, "Andrey", "example@mail.ru");
+        followee = new UserDto(2L, "Alexei", "another@mail.ru");
+        pattern = new byte[]{};
+    }
+
+    @Test
+    public void onMessageTest() throws IOException {
+        Mockito.doNothing().when(analyticsEventService).save(analyticsEvent);
+        Mockito.when(analyticsEventMapper.toEntity(followerEvent)).thenReturn(analyticsEvent);
+        Mockito.when(userServiceClient.getUser(1L)).thenReturn(follower);
+        Mockito.when(userServiceClient.getUser(2L)).thenReturn(followee);
+        Mockito.when(objectMapper.readValue(message.getBody(), FollowerEvent.class)).thenReturn(followerEvent);
+        followEventListener.onMessage(message, pattern);
+        Mockito.verify(analyticsEventService).save(analyticsEvent);
+        Mockito.verify(userServiceClient).getUser(1L);
+        Mockito.verify(userServiceClient).getUser(2L);
+        Mockito.verify(objectMapper).readValue(message.getBody(), FollowerEvent.class);
+    }
+}
