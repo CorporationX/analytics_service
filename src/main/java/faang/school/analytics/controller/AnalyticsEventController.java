5e92d44794bc80e282ccc75c2b1bce3b6f8ebe04
Nikita Volzhin <vlzh.nik@mail.ru>
2024-08-20 22:55:53 +0500

    BJS2-25775

--- src/main/java/faang/school/analytics/controller/AnalyticsEventController.java
+++ src/main/java/faang/school/analytics/controller/AnalyticsEventController.java
@@ -1,14 +1,11 @@
 package faang.school.analytics.controller;
 
 import faang.school.analytics.dto.AnalyticsEventDto;
-import faang.school.analytics.model.EventType;
+import faang.school.analytics.dto.AnalyticsEventFilterDto;
+import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.service.AnalyticsEventService;
 import lombok.RequiredArgsConstructor;
-import org.joda.time.Interval;
-import org.joda.time.LocalDateTime;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +15,12 @@ public class AnalyticsEventController {
     private final AnalyticsEventService analyticsEventService;
 
     @GetMapping("/analytics")
-    public List<AnalyticsEventDto> getAnalytics(
-            @RequestParam long receiverId, @RequestParam int eventTypeId,
-            @RequestParam(required = false) String interval,
-            @RequestParam(required = false) LocalDateTime from,
-            @RequestParam(required = false) LocalDateTime to) {
-        return analyticsEventService.getAnalytics(receiverId,
-                EventType.of(eventTypeId), interval != null ? Interval.parse(interval) : null, from, to);
+    public List<AnalyticsEventDto> getAnalytics(@RequestBody AnalyticsEventFilterDto analyticsEventFilterDto) {
+        return analyticsEventService.getAnalytics(analyticsEventFilterDto);
+    }
+
+    @PostMapping("/analytics")
+    public void save(@RequestBody AnalyticsEvent analyticsEvent){
+        analyticsEventService.saveEvent(analyticsEvent);
     }
 }
