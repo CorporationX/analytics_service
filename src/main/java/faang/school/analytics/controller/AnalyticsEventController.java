7c5426fac7466004cbca04e460ee3c8775709a01
Grisha Tkach <grisatkac@mail.ru>
2024-08-21 10:30:56 +0300

    update returned value and map request parameters into object

--- src/main/java/faang/school/analytics/controller/AnalyticsEventController.java
+++ src/main/java/faang/school/analytics/controller/AnalyticsEventController.java
@@ -1,20 +1,15 @@
 package faang.school.analytics.controller;
 
 import faang.school.analytics.dto.event.AnalyticsEventDto;
-import faang.school.analytics.model.EventType;
-import faang.school.analytics.model.Interval;
+import faang.school.analytics.dto.event.AnalyticsFilterDto;
 import faang.school.analytics.service.AnalyticsEventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping("/analytics")
@@ -25,12 +20,8 @@ public class AnalyticsEventController {
 
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public List<AnalyticsEventDto> getAnalyticsEvent(@RequestParam Long receiverId,
-                                                     @RequestParam String eventType,
-                                                     @RequestParam(required = false) String interval,
-                                                     @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime from,
-                                                     @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")  LocalDateTime to) {
+    public Page<AnalyticsEventDto> getAnalyticsEvent(AnalyticsFilterDto analyticsFilterDto) {
 
-        return analyticsEventService.getAnalytics(receiverId, EventType.of(eventType), Interval.of(interval), from, to);
+        return analyticsEventService.getAnalytics(analyticsFilterDto);
     }
 }
