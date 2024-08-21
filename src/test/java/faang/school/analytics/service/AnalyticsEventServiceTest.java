412708fbb261a3c5eb0f08302648f7afae63305c
Grisha Tkach <grisatkac@mail.ru>
2024-08-21 10:33:08 +0300

    update tests

--- src/test/java/faang/school/analytics/service/AnalyticsEventServiceTest.java
+++ src/test/java/faang/school/analytics/service/AnalyticsEventServiceTest.java
@@ -2,28 +2,44 @@ package faang.school.analytics.service;
 
 import faang.school.analytics.config.context.UserContext;
 import faang.school.analytics.dto.event.AnalyticsEventDto;
+import faang.school.analytics.dto.event.AnalyticsFilterDto;
+import faang.school.analytics.dto.event.SortField;
 import faang.school.analytics.mapper.AnalyticsEventMapper;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.model.EventType;
 import faang.school.analytics.model.Interval;
 import faang.school.analytics.repository.AnalyticsEventRepository;
+import faang.school.analytics.repository.filter.AnalyticsFilterRepository;
+import faang.school.analytics.repository.filter.TimeRangeFilterSpecification;
 import faang.school.analytics.validator.analyticsEvent.AnalyticsEventValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class AnalyticsEventServiceTest {
 
     @Mock
@@ -38,62 +54,49 @@ public class AnalyticsEventServiceTest {
     @Mock
     private UserContext userContext;
 
-    @InjectMocks
+    private AnalyticsFilterRepository timeRangeFilter;
+
     private AnalyticsEventService analyticsEventService;
 
+    private AnalyticsEvent analyticsEvent;
+    private AnalyticsEventDto analyticsEventDto;
+    private AnalyticsFilterDto filter;
+    private List<AnalyticsFilterRepository> analyticsFilterRepositories;
+
     @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
+    public void init() {
+        //MockitoAnnotations.openMocks(this);
+        Interval interval = Interval.LAST_7_DAYS;
+        filter = AnalyticsFilterDto.builder()
+                .receiverId(1L)
+                .from(interval.getStartTime())
+                .to(interval.getEndTime())
+                .page(0)
+                .size(10)
+                .sortField(SortField.RECEIVED_AT)
+                .direction(Sort.Direction.ASC)
+                .build();
+
+        analyticsEvent = new AnalyticsEvent();
+        analyticsEventDto = new AnalyticsEventDto();
+
+        timeRangeFilter = new TimeRangeFilterSpecification();
+        analyticsFilterRepositories = Arrays.asList(timeRangeFilter);
+
+        analyticsEventService = new AnalyticsEventService(analyticsEventRepository, analyticsEventValidator, analyticsEventMapper, userContext, analyticsFilterRepositories);
     }
 
     @Test
     public void testGetAnalyticsWithInterval() {
-        Long receiverId = 1L;
-        EventType eventType = EventType.ACHIEVEMENT_RECEIVED;
-        Interval interval = Interval.LAST_7_DAYS;
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime startTime = interval.getStartTime();
-        LocalDateTime endTime = interval.getEndTime();
+        Page<AnalyticsEvent> expectedEventPage = new PageImpl<>(Collections.singletonList(analyticsEvent));
 
-        AnalyticsEvent event = new AnalyticsEvent();
-        event.setReceivedAt(now.minusDays(1));
-        List<AnalyticsEvent> events = List.of(event);
-        List<AnalyticsEventDto> dtos = List.of(new AnalyticsEventDto());
+        when(analyticsEventRepository.findAll(any(Specification.class), any(Pageable.class))).thenReturn(expectedEventPage);
 
-        when(analyticsEventRepository.findByReceiverIdAndEventType(receiverId, eventType))
-                .thenReturn(events.stream());
-        when(analyticsEventMapper.toDto(any(AnalyticsEvent.class)))
-                .thenReturn(new AnalyticsEventDto());
+        Page<AnalyticsEventDto> actual = analyticsEventService.getAnalytics(filter);
 
-        List<AnalyticsEventDto> result = analyticsEventService.getAnalytics(receiverId, eventType, interval, null, null);
-
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        verify(analyticsEventRepository).findByReceiverIdAndEventType(receiverId, eventType);
-    }
-
-    @Test
-    public void testGetAnalyticsWithCustomDateRange() {
-        Long receiverId = 1L;
-        EventType eventType = EventType.ACHIEVEMENT_RECEIVED;
-        LocalDateTime from = LocalDateTime.now().minusDays(2);
-        LocalDateTime to = LocalDateTime.now().minusHours(1);
-
-        AnalyticsEvent event = new AnalyticsEvent();
-        event.setReceivedAt(LocalDateTime.now().minusDays(1));
-        List<AnalyticsEvent> events = List.of(event);
-        List<AnalyticsEventDto> dtos = List.of(new AnalyticsEventDto());
-
-        when(analyticsEventRepository.findByReceiverIdAndEventType(receiverId, eventType))
-                .thenReturn(events.stream());
-        when(analyticsEventMapper.toDto(any(AnalyticsEvent.class)))
-                .thenReturn(new AnalyticsEventDto());
-
-        List<AnalyticsEventDto> result = analyticsEventService.getAnalytics(receiverId, eventType, null, from, to);
-
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        verify(analyticsEventRepository).findByReceiverIdAndEventType(receiverId, eventType);
+        assertNotNull(actual);
+        assertEquals(1, actual.getContent().size());
+        verify(analyticsEventRepository, times(1)).findAll(any(Specification.class), any(Pageable.class));
     }
 
     @Test
