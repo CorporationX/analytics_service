34defe1f83dc3451a5f642aa718aed38f45e3a11
Grisha Tkach <grisatkac@mail.ru>
2024-08-21 10:32:59 +0300

    update service

--- src/main/java/faang/school/analytics/service/AnalyticsEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -2,20 +2,26 @@ package faang.school.analytics.service;
 
 import faang.school.analytics.config.context.UserContext;
 import faang.school.analytics.dto.event.AnalyticsEventDto;
+import faang.school.analytics.dto.event.AnalyticsFilterDto;
+import faang.school.analytics.dto.event.SortField;
+import faang.school.analytics.exception.DataValidationException;
 import faang.school.analytics.mapper.AnalyticsEventMapper;
 import faang.school.analytics.model.AnalyticsEvent;
-import faang.school.analytics.model.EventType;
-import faang.school.analytics.model.Interval;
 import faang.school.analytics.repository.AnalyticsEventRepository;
+import faang.school.analytics.repository.filter.AnalyticsFilterRepository;
 import faang.school.analytics.validator.analyticsEvent.AnalyticsEventValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,25 +31,32 @@ public class AnalyticsEventService {
     private final AnalyticsEventValidator analyticsEventValidator;
     private final AnalyticsEventMapper analyticsEventMapper;
     private final UserContext userContext;
+    private final List<AnalyticsFilterRepository> analyticsFilterRepositories;
 
     @Transactional
-    public List<AnalyticsEventDto> getAnalytics(Long receiverId, EventType eventType, Interval interval, LocalDateTime from, LocalDateTime to) {
-        analyticsEventValidator.allDateIntervalsAreEmpty(interval, from, to);
+    public Page<AnalyticsEventDto> getAnalytics(AnalyticsFilterDto analyticsFilter) {
+        LocalDateTime startTime = analyticsFilter.getFrom();
+        LocalDateTime endTime = analyticsFilter.getTo();
+        analyticsEventValidator.allDateIntervalsAreEmpty(analyticsFilter.getInterval(), startTime, endTime);
 
-        if (interval == null) {
-            analyticsEventValidator.customIntervalIsValid(from, to);
+        if (analyticsFilter.getInterval() == null) {
+            analyticsEventValidator.customIntervalIsValid(startTime, endTime);
         }
 
-        LocalDateTime startTime = (interval != null) ? interval.getStartTime() : from;
-        LocalDateTime endTime = (interval != null) ? interval.getEndTime() : to;
+        Optional<Specification<AnalyticsEvent>> analyticsSpecification = analyticsFilterRepositories.stream()
+                .filter(filter -> filter.isApplicable(analyticsFilter))
+                .map(filter -> filter.apply(analyticsFilter))
+                .reduce(Specification::and);
 
-        Stream<AnalyticsEvent> byReceiverIdAndEventType = analyticsEventRepository.findByReceiverIdAndEventType(receiverId, eventType);
+        analyticsSpecification.orElseThrow(() -> new DataValidationException("Required fields are incorrect"));
 
-        return byReceiverIdAndEventType
-                .filter(event -> event.getReceivedAt().isAfter(startTime) && event.getReceivedAt().isBefore(endTime))
-                .sorted(Comparator.comparing(AnalyticsEvent::getReceivedAt).reversed())
-                .map(analyticsEventMapper::toDto)
-                .toList();
+        Pageable pageRequest = preparePageRequest(
+                analyticsFilter.getPage(),
+                analyticsFilter.getSize(),
+                analyticsFilter.getSortField(),
+                analyticsFilter.getDirection());
+
+        return  analyticsEventRepository.findAll(analyticsSpecification.get(), pageRequest).map(analyticsEventMapper::toDto);
     }
 
     @Transactional
@@ -63,5 +76,11 @@ public class AnalyticsEventService {
                 : analyticsEvent.getReceivedAt());
 
         return analyticsEvent;
+    }
+
+    public Pageable preparePageRequest(int page, int size, SortField sortField, Sort.Direction direction) {
+        Sort sort = Sort.by(SortField.valueOf(sortField.name()).getValue());
+        sort = (direction.isAscending()) ? sort.ascending() : sort.descending();
+        return PageRequest.of(page, size, sort);
     }
 }
