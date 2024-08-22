dc498e04392338b29c0a46b56cba1036b51b15bf
Nikita Volzhin <vlzh.nik@mail.ru>
2024-08-22 15:23:54 +0500

    change AnalyticsEventControllers

--- src/main/java/faang/school/analytics/controller/AnalyticsEventController.java
+++ src/main/java/faang/school/analytics/controller/AnalyticsEventController.java
@@ -6,6 +6,7 @@ import faang.school.analytics.service.AnalyticsEventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
