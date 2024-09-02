f566d3562ac03e4582e0543c4d10e6f864e69f81
grisatkac <grisatkac@mail.ru>
2024-09-03 00:34:45 +0300

    combine analytic controller

--- src/main/java/faang/school/analytics/controller/AnalyticController.java
+++ src/main/java/faang/school/analytics/controller/AnalyticController.java
@@ -2,15 +2,15 @@ package faang.school.analytics.controller;
 
 import faang.school.analytics.dto.AnalyticsEventDto;
 import faang.school.analytics.dto.AnalyticsEventRequestDto;
+import faang.school.analytics.dto.event.AnalyticsFilterDto;
 import faang.school.analytics.exception.handler.BadRequestException;
 import faang.school.analytics.service.AnalyticsEventService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * @author Evgenii Malkov
@@ -22,13 +22,11 @@ public class AnalyticController {
 
     public final AnalyticsEventService analyticsEventService;
 
-    @GetMapping
-    public List<AnalyticsEventDto> getAnalytics(@ModelAttribute @Validated AnalyticsEventRequestDto request) {
-        if (request.getInterval() == null && (request.getFrom() == null || request.getTo() == null)) {
-            throw new BadRequestException("No period filter value specified");
-        }
-        return analyticsEventService.getAnalytics(request.getId(), request.getEventType(),
-                request.getInterval(), request.getFrom(), request.getTo());
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public Page<AnalyticsEventDto> getAnalyticsEvent(AnalyticsFilterDto analyticsFilterDto) {
+
+        return analyticsEventService.getAnalytics(analyticsFilterDto);
     }
 
     @PostMapping
