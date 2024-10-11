296a6c4275796bd7e1959218c928cb9d4ecdd67a
Anton Martynenko <ant.martynenko@gmail.com>
2024-10-11 17:39:32 +0200

    Задача "Анализ отображения профиля пользователя в запросах с фильтрами"

---
+++ src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
@@ -0,0 +1,30 @@
+package faang.school.analytics.config.redis;
+
+import faang.school.analytics.listener.SearchAppearanceEventListener;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfiguration {
+    private final RedisConnectionFactory connectionFactory;
+    private final SearchAppearanceEventListener searchAppearanceEventListener;
+    private List<String> topics;
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer() {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        MessageListenerAdapter searchAppearanceEventListenerAdapter = new MessageListenerAdapter(searchAppearanceEventListener);
+        container.addMessageListener(searchAppearanceEventListenerAdapter, new ChannelTopic("search-appearance"));
+        return container;
+    }
+}
