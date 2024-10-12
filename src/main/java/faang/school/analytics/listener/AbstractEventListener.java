9eefb1f83249d438df14a4c412668475dc039560
vestmen <alex.nemtsev95@gmail.com>
2024-10-12 22:22:14 +0300

    BJS2-27097 Анализ просмотров постов
    # Conflicts:
    #	src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java

---
+++ src/main/java/faang/school/analytics/listener/AbstractEventListener.java
@@ -0,0 +1,36 @@
+package faang.school.analytics.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.analytics.mapper.AnalyticsEventMapper;
+import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.service.AnalyticsEventService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.function.Function;
+
+@Slf4j
+@Getter
+@Component
+@RequiredArgsConstructor
+public abstract class AbstractEventListener<T> {
+    private final ObjectMapper objectMapper;
+    private final AnalyticsEventService analyticsEventService;
+    private final AnalyticsEventMapper mapper;
+
+    protected void saveEvent(Message message, Class<T> cl, Function<T, AnalyticsEvent> analyticsEventFunction) {
+        try {
+            T eventDto = objectMapper.readValue(message.getBody(), cl);
+            AnalyticsEvent event = analyticsEventFunction.apply(eventDto);
+            analyticsEventService.saveEvent(event);
+            log.info("Event saved {}", event);
+        } catch (IOException e) {
+            log.error("Object mapper unread {} message error:", cl.getName(), e);
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/faang/school/analytics/redis/RedisConfig.java
@@ -0,0 +1,67 @@
+package faang.school.analytics.redis;
+
+import faang.school.analytics.listener.PostViewEventListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.channel.post-view}")
+    private String postViewEventTopic;
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+        template.setEnableTransactionSupport(true);
+
+        return template;
+    }
+
+    @Bean
+    public MessageListenerAdapter listenerAdapter(PostViewEventListener listener) {
+        return new MessageListenerAdapter(listener);
+    }
+
+    @Bean
+    public RedisMessageListenerContainer container(JedisConnectionFactory jedisConnectionFactory,
+                                                   MessageListenerAdapter listenerAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(jedisConnectionFactory);
+        container.addMessageListener(listenerAdapter, postViewEventTopic());
+        return container;
+    }
+
+    @Bean
+    public ChannelTopic postViewEventTopic() {
+        return new ChannelTopic(postViewEventTopic);
+    }
+}
