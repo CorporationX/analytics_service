895cc80a682b4fcc2a97926f3785d1ad030f79b7
Владислав Воронов <iPlesk90@yandex.ru>
2024-10-12 11:57:38 +0300

    Выполнил задачу

--- src/test/java/faang/school/analytics/redis/RedisMessageConsumerMentorshipRequestsTest.java
+++ src/test/java/faang/school/analytics/redis/RedisMessageConsumerMentorshipRequestsTest.java
@@ -1,8 +1,7 @@
 package faang.school.analytics.redis;
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.analytics.service.AnalyticsEventService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -10,12 +9,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.connection.Message;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -26,46 +25,32 @@ public class RedisMessageConsumerMentorshipRequestsTest {
     private AnalyticsEventService analyticsEventService;
 
     @Mock
-    private Message message;
+    private ObjectMapper objectMapper;
 
     @InjectMocks
     private RedisMessageConsumerMentorshipRequests redisMessageConsumerMentorshipRequests;
-
-
-    @BeforeEach
-    public void setUp() {
-        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.findAndRegisterModules();
-        objectMapper.registerModule(new JavaTimeModule());
-    }
 
     @Test
     public void testOnMessage() throws IOException {
         String json = "{\"requesterId\": 12345, \"receiverId\": 67890, \"requestTime\": \"2024-10-12T10:00:00\"}";
 
-        // Создаем объект MentorshipRequestEvent, который мы ожидаем получить после десериализации
+        Message message = mock(Message.class);
         MentorshipRequestEvent expectedEvent = new MentorshipRequestEvent();
         expectedEvent.setRequesterId(12345L);
         expectedEvent.setReceiverId(67890L);
         expectedEvent.setRequestTime(LocalDateTime.parse("2024-10-12T10:00:00"));
 
-        // Мокаем поведение message
         byte[] nameChannel = "nameChannel".getBytes();
         byte[] messageBytes = json.getBytes();
 
-        when(message.getBody()).thenReturn(messageBytes); // Мокируем тело сообщения
+        when(objectMapper.readValue(messageBytes, MentorshipRequestEvent.class)).thenReturn(expectedEvent);
+        when(message.getBody()).thenReturn(messageBytes);
         when(message.getChannel()).thenReturn(nameChannel);
-        // Вызов метода onMessage, который должен десериализовать JSON и сохранить событие
+
         redisMessageConsumerMentorshipRequests.onMessage(message, null);
 
-        // Каптурим переданное событие
         ArgumentCaptor<MentorshipRequestEvent> eventCaptor = ArgumentCaptor.forClass(MentorshipRequestEvent.class);
         verify(analyticsEventService).saveAnalyticEvent(eventCaptor.capture());
-
-        // Проверяем, что сохраненный объект совпадает с ожидаемым
-        assertEquals(expectedEvent.getRequesterId(), eventCaptor.getValue().getRequesterId());
-        assertEquals(expectedEvent.getReceiverId(), eventCaptor.getValue().getReceiverId());
-        assertEquals(expectedEvent.getRequestTime(), eventCaptor.getValue().getRequestTime());
+        assertEquals(expectedEvent, eventCaptor.getValue());
     }
 }
-
