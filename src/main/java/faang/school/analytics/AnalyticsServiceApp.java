68cacd6b19534001d00dcc517a24a88b42ad2289
Ilia Chudov <chudow-iluha@mail.ru>
2024-10-13 00:39:07 +0300

    BJS2-27102 Анализ комментариев
    Небольшие изменения

--- src/main/java/faang/school/analytics/AnalyticsServiceApp.java
+++ src/main/java/faang/school/analytics/AnalyticsServiceApp.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRetry
 @EnableFeignClients("faang.school.analytics.client")
 public class AnalyticsServiceApp {
     public static void main(String[] args) {
