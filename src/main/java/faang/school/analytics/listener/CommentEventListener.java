3656e13983fe6ebe8d292662430ea5c4f9794b49
Peregruzochka <peregruzochka48@mail.ru>
2024-10-13 17:42:19 +0300

    настроено получение сообщений из redis

---
+++ src/main/java/faang/school/analytics/listener/CommentEventListener.java
@@ -0,0 +1,38 @@
+package faang.school.analytics.listener;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.analytics.dto.CommentEventDto;
+import faang.school.analytics.mapper.AnalyticsCommentEventMapper;
+import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.service.AnalyticsEventService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CommentEventListener implements MessageListener {
+    private final ObjectMapper objectMapper;
+    private final AnalyticsCommentEventMapper commentEventMapper;
+    private final AnalyticsEventService analyticsService;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            CommentEventDto commentEventDto = objectMapper.readValue(message.getBody(), CommentEventDto.class);
+            AnalyticsEvent analyticsEvent = commentEventMapper.toAnalyticsEvent(commentEventDto);
+            AnalyticsEvent savedAnalyticsEvent = analyticsService.saveEvent(analyticsEvent);
+            log.info("Received new analytics data: {}", savedAnalyticsEvent);
+
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Error with mapping to CommentEventDto");
+        }
+
+    }
+}
