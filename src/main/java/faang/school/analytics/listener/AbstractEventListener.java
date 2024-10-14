0d1c201a6f10cb54dfe977ce5e88f2af60f20f90
vestmen <alex.nemtsev95@gmail.com>
2024-10-14 11:13:58 +0300

    BJS2-27097 Анализ просмотров внес правки после кросс-ревью

--- src/main/java/faang/school/analytics/listener/AbstractEventListener.java
+++ src/main/java/faang/school/analytics/listener/AbstractEventListener.java
@@ -8,16 +8,15 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
-import org.springframework.stereotype.Component;
+import org.springframework.data.redis.connection.MessageListener;
 
 import java.io.IOException;
 import java.util.function.Function;
 
 @Slf4j
 @Getter
-@Component
 @RequiredArgsConstructor
-public abstract class AbstractEventListener<T> {
+public abstract class AbstractEventListener<T> implements MessageListener {
     private final ObjectMapper objectMapper;
     private final AnalyticsEventService analyticsEventService;
     private final AnalyticsEventMapper mapper;
