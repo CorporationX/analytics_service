44a1c074837cc81034ee941f7d93667c63ae9c46
SharkB38 <val.wolf.ne@gmail.com>
2024-10-14 17:18:17 +0900

    refactor code

--- src/main/java/faang/school/analytics/config/redis/RedisConfig.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfig.java
@@ -1,28 +1,26 @@
 package faang.school.analytics.config.redis;
 
 import faang.school.analytics.listener.FollowerEventListener;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Setter
 @Configuration
+@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "spring.data.redis")
 public class RedisConfig {
     private int port;
     private String host;
 
-    @Value("${spring.data.redis.channel.follower-event-channel}")
-    private String followerEventTopic;
+    private final RedisChannelNames redisChannelNames;
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
@@ -32,17 +30,8 @@ public class RedisConfig {
     }
 
     @Bean
-    RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        return template;
-    }
-
-    @Bean
-    MessageListenerAdapter followerListener(FollowerEventListener goalCompletedListener) {
-        return new MessageListenerAdapter(goalCompletedListener);
+    MessageListenerAdapter followerListener(FollowerEventListener followerEventListener) {
+        return new MessageListenerAdapter(followerEventListener);
     }
 
     @Bean
@@ -56,6 +45,6 @@ public class RedisConfig {
 
     @Bean
     ChannelTopic followerEventTopic() {
-        return new ChannelTopic(followerEventTopic);
+        return new ChannelTopic(redisChannelNames.getFollowerEventChannel());
     }
 }
