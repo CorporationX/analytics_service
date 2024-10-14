1e3d7c5359bae4716d2eca7e7c919e51fa9e7496
vestmen <alex.nemtsev95@gmail.com>
2024-10-14 11:13:58 +0300

    BJS2-27097 Анализ просмотров внес правки после кросс-ревью

--- src/main/java/faang/school/analytics/listener/PostViewEventListener.java
+++ src/main/java/faang/school/analytics/listener/PostViewEventListener.java
@@ -7,14 +7,13 @@ import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.service.AnalyticsEventService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @Slf4j
 @Service
-public class PostViewEventListener extends AbstractEventListener<PostViewEventDto> implements MessageListener {
+public class PostViewEventListener extends AbstractEventListener<PostViewEventDto> {
     public PostViewEventListener(ObjectMapper objectMapper,
                                  AnalyticsEventService analyticsEventService,
                                  AnalyticsEventMapper mapper) {
