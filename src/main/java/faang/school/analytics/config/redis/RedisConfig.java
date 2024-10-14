0253fcd97173ad2b9851e93d15bc786113e0e543
SharkB38 <val.wolf.ne@gmail.com>
2024-10-14 18:06:19 +0900

    remove host and port configuration for redis

--- src/main/java/faang/school/analytics/config/redis/RedisConfig.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfig.java
@@ -3,10 +3,8 @@ package faang.school.analytics.config.redis;
 import faang.school.analytics.listener.FollowerEventListener;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -15,18 +13,12 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 @Setter
 @Configuration
 @RequiredArgsConstructor
-@ConfigurationProperties(prefix = "spring.data.redis")
 public class RedisConfig {
-    private int port;
-    private String host;
-
     private final RedisChannelNames redisChannelNames;
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration configuration =
-                new RedisStandaloneConfiguration(host, port);
-        return new JedisConnectionFactory(configuration);
+        return new JedisConnectionFactory();
     }
 
     @Bean
