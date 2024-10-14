a8c26e417c9ccfa44c6ced4d023f99e858abf1fe
alibajsarov <alibajsarov353@gmail.com>
2024-10-15 02:13:48 +0400

    Задача на стороне слушателя (Без тестов)

--- src/main/java/faang/school/analytics/listener/RecommendationEventListener.java
+++ src/main/java/faang/school/analytics/listener/RecommendationEventListener.java
@@ -6,10 +6,11 @@ import faang.school.analytics.model.dto.RecommendationEventDto;
 import faang.school.analytics.model.entity.AnalyticsEvent;
 import faang.school.analytics.model.enums.EventType;
 import faang.school.analytics.service.AnalyticsEventService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
-
+@Slf4j
 @Component
 public class RecommendationEventListener extends AbstractEventListener<RecommendationEventDto> implements MessageListener {
     public RecommendationEventListener(AnalyticsEventService analyticsEventService,
@@ -20,6 +21,7 @@ public class RecommendationEventListener extends AbstractEventListener<Recommend
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
+        log.info("message received! {}",message);
         RecommendationEventDto event = handleEvent(message, RecommendationEventDto.class);
         AnalyticsEvent entity = analyticsEventMapper.toEntity(event);
         entity.setEventType(EventType.RECOMMENDATION_RECEIVED);
--- src/main/java/faang/school/analytics/model/dto/RecommendationEventDto.java
+++ src/main/java/faang/school/analytics/model/dto/RecommendationEventDto.java
@@ -6,11 +6,12 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
 @Builder
-public class RecommendationEventDto {
-    private Long authorId;
-    private Long receiverId;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-    private LocalDateTime recommendedAt;
+public record RecommendationEventDto(
+        Long authorId,
+        Long receiverId,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        LocalDateTime recommendedAt
+) {
+
 }
