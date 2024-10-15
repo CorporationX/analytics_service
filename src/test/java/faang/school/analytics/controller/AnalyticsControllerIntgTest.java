d99eef0f57d259702102b826a88cfc90dc12a4ea
Denis Artamonov <dns.artamonov@gmail.com>
2024-10-15 17:11:09 +0300

    BJS2-27285

--- src/test/java/faang/school/analytics/controller/AnalyticsControllerIntgTest.java
+++ src/test/java/faang/school/analytics/controller/AnalyticsControllerIntgTest.java
@@ -1,5 +1,6 @@
 package faang.school.analytics.controller;
 
+import com.jayway.jsonpath.JsonPath;
 import faang.school.analytics.config.context.UserContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,15 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.sql.DataSource;
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -31,7 +33,6 @@ class AnalyticsControllerIntgTest {
     @MockBean
     UserContext userContext;
 
-    private JdbcTemplate jdbc;
     @Container
     private static final PostgreSQLContainer<?> postgresContainer =
             new PostgreSQLContainer<>("postgres:latest")
@@ -42,25 +43,53 @@ class AnalyticsControllerIntgTest {
 
     @BeforeEach
     public void setUp() {
-        DataSource dataSource = new DriverManagerDataSource(postgresContainer.getJdbcUrl(),
-                postgresContainer.getUsername(), postgresContainer.getPassword());
-        jdbc = new JdbcTemplate(dataSource);
     }
 
+    @DynamicPropertySource
+    static void overrideSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgresContainer::getDriverClassName);
+        registry.add("spring.liquibase.enabled", () -> false);
+    }
 
     @Test
     void testGetAnalytics_withValidParams() throws Exception {
-        String interval = "WEEK";
-        Long id = 100L;
-        String eventType = "FOLLOWER";
-        mockMvc.perform(MockMvcRequestBuilders.get("/analytics")
+        String startDate = "2024-10-09T16:37:34.252454600";
+        String endDate = "2024-10-11T16:37:34.252454600";
+        Long id = 3L;
+        String eventType = "PROFILE_APPEARED_IN_SEARCH";
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/analytics")
                         .header("x-user-id", 8L)
                         .param("id", id.toString())
                         .param("eventType", eventType)
-                        .param("interval", interval))
+                        .param("startDate", startDate)
+                        .param("endDate", endDate))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$").isArray());
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].eventType").value(eventType))
+                .andExpect(jsonPath("$[0].receiverId").value(id))
+                .andExpect(jsonPath("$[1].eventType").value(eventType))
+                .andExpect(jsonPath("$[1].receiverId").value(id))
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+
+        LocalDateTime receivedAtFirstObject = JsonPath.parse(jsonResponse).read("$[0].receivedAt", LocalDateTime.class);
+//        String receivedAtFirstObject = JsonPath.parse(jsonResponse).read("$[0].eventType");
+        // TODO
+        System.out.println("receiveeeeeeeeeeeeeeeeeeeeeeeeeeee"+receivedAtFirstObject);
+        LocalDateTime receivedAtSecondObject = JsonPath.parse(jsonResponse).read("$[1].receivedAt", LocalDateTime.class);
+
+//        Assertions.assertAll(
+//                ()->Assertions.assertTrue(receivedAtFirstObject.isAfter(LocalDateTime.parse(startDate))),
+//                ()->Assertions.assertTrue(receivedAtFirstObject.isBefore(LocalDateTime.parse(endDate))),
+//                ()->Assertions.assertTrue(receivedAtSecondObject.isAfter(LocalDateTime.parse(startDate))),
+//                ()->Assertions.assertTrue(receivedAtSecondObject.isBefore(LocalDateTime.parse(endDate)))
+//        );
     }
 }
