88c67db442cdd17028c17fab5a10eb14ab3c79ad
vestmen <alex.nemtsev95@gmail.com>
2024-10-18 15:02:23 +0300

    BJS2-27097 Анализ просмотров постов Вынес создание контейнеров в отдельный пакет и внес правки

--- src/main/java/faang/school/analytics/config/context/redis/RedisConfig.java
+++ src/main/java/faang/school/analytics/config/context/redis/RedisConfig.java
@@ -1,22 +1,17 @@
 package faang.school.analytics.config.context.redis;
 
-import faang.school.analytics.listener.PostViewEventListener;
+import faang.school.analytics.config.context.redis.listener_containers.RedisContainerMessageListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.List;
 
 @Configuration
 public class RedisConfig {
-    @Value("${spring.data.redis.channel.post-view}")
-    private String postViewEventTopic;
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -31,37 +26,13 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
-
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(serializer);
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(serializer);
-        template.setEnableTransactionSupport(true);
-
-        return template;
-    }
-
-    @Bean
-    public MessageListenerAdapter listenerAdapter(PostViewEventListener listener) {
-        return new MessageListenerAdapter(listener);
-    }
-
-    @Bean
-    public RedisMessageListenerContainer container(JedisConnectionFactory jedisConnectionFactory,
-                                                   MessageListenerAdapter listenerAdapter) {
+    public RedisMessageListenerContainer redisContainer(List<RedisContainerMessageListener> messageListeners) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(jedisConnectionFactory);
-        container.addMessageListener(listenerAdapter, postViewEventTopic());
-        return container;
-    }
+        container.setConnectionFactory(jedisConnectionFactory());
 
-    @Bean
-    public ChannelTopic postViewEventTopic() {
-        return new ChannelTopic(postViewEventTopic);
+        messageListeners.forEach(messageListener -> container
+                .addMessageListener(messageListener.getAdapter(), messageListener.getTopic()));
+
+        return container;
     }
 }
