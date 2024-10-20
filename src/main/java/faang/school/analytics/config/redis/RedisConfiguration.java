f16be06ea5024612a87c73e79fd5d60c16ed2db6
Sergey Sklyar <sklyar1091@gmail.com>
2024-10-21 00:27:41 +0300

    - conflicts resolving

--- src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
@@ -1,7 +1,7 @@
 package faang.school.analytics.config.redis;
 
-import faang.school.analytics.listener.project.ProjectViewEventListener;
 import faang.school.analytics.listener.like.LikeEventListener;
+import faang.school.analytics.listener.project.ProjectViewEventListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,13 +14,16 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 @RequiredArgsConstructor
 public class RedisConfiguration {
 
-<<<<<<< HEAD
-    private final RedisProperties propertiesConfig;
-=======
     private final RedisProperties redisProperties;
 
     @Bean
-    RedisMessageListenerContainer redisContainer(MessageListenerAdapter projectViewEvent) {
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer(MessageListenerAdapter projectViewEvent,
+                                                 MessageListenerAdapter likeEventAdapter) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(jedisConnectionFactory());
         container.addMessageListener(projectViewEvent, projectViewEventTopic());
@@ -34,44 +37,13 @@ public class RedisConfiguration {
     }
 
     @Bean
-    MessageListenerAdapter projectViewEvent(ProjectViewEventListener projectViewEventListener) {
-        return new MessageListenerAdapter(projectViewEventListener);
-    }
->>>>>>> unicorn-master-stream6
-
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public MessageListenerAdapter likeEventAdapter(LikeEventListener likeEventListener) {
-        return new MessageListenerAdapter(likeEventListener);
-    }
-
-    @Bean
     public ChannelTopic likeEventsTopic() {
-        return new ChannelTopic(propertiesConfig.getChannel().getLikeEvents());
-    }
-
-    @Bean
-    ChannelTopic projectViewEventTopic() {
-        return new ChannelTopic(redisProperties.getChannel().getProjectViewChannel());
+        return new ChannelTopic(redisProperties.getChannel().getLikeEvents());
     }
 
     @Bean
     MessageListenerAdapter projectViewEvent(ProjectViewEventListener projectViewEventListener) {
         return new MessageListenerAdapter(projectViewEventListener);
-    }
-
-<<<<<<< HEAD
-
-    @Bean
-    public RedisMessageListenerContainer redisContainer(MessageListenerAdapter likeEventAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(jedisConnectionFactory());
-        container.addMessageListener(likeEventAdapter, likeEventsTopic());
-        return container;
     }
 
     @Bean
