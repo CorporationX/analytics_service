851233b83dd89bf598760c78b4f0b84d7e080265
Ilia Chudov <chudow-iluha@mail.ru>
2024-10-21 12:09:00 +0300

    BJS2-37866  Разработать интеграционный тест в analytics_service
    Правки после проверки джакоко

--- src/test/java/faang/school/analytics/redis/listener/CommentEventListenerTest.java
+++ src/test/java/faang/school/analytics/redis/listener/CommentEventListenerTest.java
@@ -1,5 +1,6 @@
 package faang.school.analytics.redis.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.analytics.dto.comment.CommentEvent;
 import faang.school.analytics.mapper.AnalyticsEventMapper;
@@ -10,9 +11,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.connection.Message;
 
 import java.time.LocalDateTime;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -49,5 +52,18 @@ class CommentEventListenerTest {
 
         verify(analyticsEventMapper, times(1)).toAnalyticsEvent(commentEvent);
         verify(analyticsEventService, times(1)).saveEvent(analyticsEvent);
+    }
+    @Test
+    void testOnMessageWithInvalidJson() throws Exception {
+        String invalidJson = "invalid json";
+        Message message = mock(Message.class);
+        when(message.getBody()).thenReturn(invalidJson.getBytes());
+
+        when(objectMapper.readValue(invalidJson, CommentEvent.class))
+                .thenThrow(new JsonProcessingException("Invalid JSON") {});
+
+        commentEventListener.onMessage(message, null);
+
+        verify(objectMapper, times(1)).readValue(invalidJson, CommentEvent.class);
     }
 }
