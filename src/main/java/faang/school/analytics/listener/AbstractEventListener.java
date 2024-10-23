5730d01ba238834016f0eb3394461c97c7bc8079
mamadjonovKO <mamadjonov_k@auca.kg>
2024-10-23 21:56:03 +0600

    Refactored according to the review

--- src/main/java/faang/school/analytics/listener/AbstractEventListener.java
+++ src/main/java/faang/school/analytics/listener/AbstractEventListener.java
@@ -3,6 +3,7 @@ package faang.school.analytics.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.analytics.exception.DeserializeException;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.function.Consumer;
 
 @NoArgsConstructor
+@Slf4j
 public abstract class AbstractEventListener<T> implements MessageListener {
     @Autowired
     protected ObjectMapper objectMapper;
@@ -21,9 +23,12 @@ public abstract class AbstractEventListener<T> implements MessageListener {
 
     protected void handleEvent(Message message, Class<T> type, Consumer<T> consumer) {
         try {
+            log.info("Received message: {}", new String(message.getBody()));
             T event = objectMapper.readValue(message.getBody(), type);
+            log.info("Successfully deserialized message to {}: {}", type.getSimpleName(), event);
             consumer.accept(event);
         } catch (IOException e) {
+            log.error("Error deserializing JSON to object: {}", e.getMessage(), e);
             throw new DeserializeException("Error deserializing JSON to object: " + e.getMessage());
         }
     }
