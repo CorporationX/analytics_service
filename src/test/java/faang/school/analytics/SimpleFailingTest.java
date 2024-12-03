1d07c7a2006a0c62c69137c061a56ecef07c1e77
UrmatBolotbek <urmar9797@gmail.com>
2024-12-03 14:26:53 +0100

    добавил тест

---
+++ src/test/java/faang/school/analytics/SimpleFailingTest.java
@@ -0,0 +1,12 @@
+package faang.school.analytics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleFailingTest {
+    @Test
+    void simpleFailingTest() {
+        assertEquals(4, 1 + 2);
+    }
+}
