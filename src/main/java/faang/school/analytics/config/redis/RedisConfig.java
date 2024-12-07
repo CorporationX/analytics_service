364d390f3d5055b900096bd6770fd30f41114068
Vakarash <max.vackarash@gmail.com>
2024-12-07 13:31:19 +0300

    небольшие изменения конфига

--- src/main/java/faang/school/analytics/config/redis/RedisConfig.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfig.java
@@ -1,14 +1,14 @@
 package faang.school.analytics.config.redis;
 
-import faang.school.analytics.event.subscriber.AnalyticsEventSubscriber;
+import faang.school.analytics.event.listener.LikeEventListener;
 import faang.school.analytics.event.publisher.AnalyticsEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,15 +16,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public MessageListenerAdapter messageListener() {
-        return new MessageListenerAdapter(new AnalyticsEventSubscriber());
+    public MessageListenerAdapter messageListener(LikeEventListener likeEventListener) {
+        return new MessageListenerAdapter(likeEventListener, "onMessage");
     }
 
     @Bean
-    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory) {
+    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
+                                                        MessageListenerAdapter messageListener) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(messageListener(), topic());
+        container.addMessageListener(messageListener, topic());
         return container;
     }
 
