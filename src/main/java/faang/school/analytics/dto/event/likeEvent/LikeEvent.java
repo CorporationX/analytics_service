e3dadc01dd7fd6849ae82bacccc4443ab2cac3e8
Vakarash <max.vackarash@gmail.com>
2024-12-08 22:02:09 +0300

    Фиксы чтобы приложение стартовало

--- src/main/java/faang/school/analytics/dto/event/likeEvent/LikeEvent.java
+++ src/main/java/faang/school/analytics/dto/event/likeEvent/LikeEvent.java
@@ -1,14 +1,10 @@
 package faang.school.analytics.dto.event.likeEvent;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Builder
-@Getter
-@Setter
 public record LikeEvent (
     Long postId,
     Long authorId,
