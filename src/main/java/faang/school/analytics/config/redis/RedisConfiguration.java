4b010d3fac4e878862e57684ce11a6cdf08391f7
lomuserg <sergeymu2015@yandex.ru>
2024-12-11 20:55:13 +0300

    BJS2-41411: fix processingError Json

--- src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
@@ -36,12 +36,11 @@ public class RedisConfiguration {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        return redisTemplate;
+    public RedisTemplate<String, Object> redisTemplate(ObjectMapper objectMapper) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(objectMapper, Object.class));
+        return template;
     }
 
 
