1e1214fd1272f7523bbaa3046d8c31434501eb8b
AVB <abdddev@gmail.com>
2024-12-13 00:57:43 +1000

    added RecommendationEvent

--- src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
@@ -1,5 +1,6 @@
-package faang.school.analytics.mapper.follower;
+package faang.school.analytics.mapper;
 
+import faang.school.analytics.event.RecommendationEvent;
 import faang.school.analytics.event.follower.FollowerEvent;
 import faang.school.analytics.model.AnalyticsEvent;
 import org.mapstruct.Mapper;
@@ -14,4 +15,9 @@ public interface AnalyticsEventMapper {
     @Mapping(target = "receiverId", source = "followeeId")
     @Mapping(target = "receivedAt", expression = "java(java.time.LocalDateTime.now())")
     AnalyticsEvent followerEventToAnalyticsEvent(FollowerEvent followerEvent);
+
+    @Mapping(target = "eventType", constant = "RECOMMENDATION_RECEIVED")
+    @Mapping(target = "actorId", source = "authorId")
+    @Mapping(target = "receivedAt", source = "dateTime")
+    AnalyticsEvent recommendationEventToAnalyticsEvent(RecommendationEvent recommendationEvent);
 }
--- src/main/java/faang/school/analytics/redis/listener/impl/RecommendationEventListener.java
+++ src/main/java/faang/school/analytics/redis/listener/impl/RecommendationEventListener.java
@@ -1,42 +1,40 @@
 package faang.school.analytics.redis.listener.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import faang.school.analytics.event.follower.FollowerEvent;
+import faang.school.analytics.event.RecommendationEvent;
 import faang.school.analytics.mapper.AnalyticsEventMapper;
-import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.redis.listener.AbstractEventListener;
 import faang.school.analytics.service.AnalyticsEventService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
-public class FollowerEventListener extends AbstractEventListener<FollowerEvent> {
+public class RecommendationEventListener extends AbstractEventListener<RecommendationEvent> {
 
-    @Value("${spring.data.redis.channel.follower-event}")
-    private String followerEventChannel;
+    @Value("${spring.data.redis.channel.recommendation}")
+    private String recommendationChannel;
 
     private final AnalyticsEventMapper analyticsEventMapper;
 
-    public FollowerEventListener(ObjectMapper objectMapper,
-                                 AnalyticsEventService analyticsEventService,
-                                 AnalyticsEventMapper analyticsEventMapper) {
+    public RecommendationEventListener(
+            ObjectMapper objectMapper,
+            AnalyticsEventService analyticsEventService,
+            AnalyticsEventMapper analyticsEventMapper
+    ) {
         super(objectMapper, analyticsEventService);
         this.analyticsEventMapper = analyticsEventMapper;
     }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        FollowerEvent followerEvent = mapMessage(message, FollowerEvent.class);
-        AnalyticsEvent analyticsEvent = analyticsEventMapper.followerEventToAnalyticsEvent(followerEvent);
-        save(analyticsEvent);
+        RecommendationEvent recommendationEvent = mapMessage(message, RecommendationEvent.class);
+        save(analyticsEventMapper.recommendationEventToAnalyticsEvent(recommendationEvent));
     }
 
     @Override
     public ChannelTopic getChannelTopic() {
-        return new ChannelTopic(followerEventChannel);
+        return new ChannelTopic(recommendationChannel);
     }
 }
