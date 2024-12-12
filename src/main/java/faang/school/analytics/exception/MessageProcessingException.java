9bab4d6c39dbfd8902868f939641ab5fb9b59696
Yevgeny Geodakyan <yevgengeodakyan@gmail.com>
2024-12-11 21:12:36 -0500

    added check for null value

--- src/main/java/faang/school/analytics/exception/MessageProcessingException.java
+++ src/main/java/faang/school/analytics/exception/MessageProcessingException.java
@@ -1,6 +1,10 @@
 package faang.school.analytics.exception;
 
 public class MessageProcessingException extends RuntimeException {
+    public MessageProcessingException(String message) {
+        super(message);
+    }
+
     public MessageProcessingException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/faang/school/analytics/listener/GoalCompletedEventListener.java
+++ src/main/java/faang/school/analytics/listener/GoalCompletedEventListener.java
@@ -7,6 +7,7 @@ import faang.school.analytics.mapper.GoalCompletedMapper;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.model.EventType;
 import faang.school.analytics.service.AnalyticsEventService;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
@@ -24,7 +25,11 @@ public class GoalCompletedEventListener implements MessageListener {
     private final GoalCompletedMapper goalCompletedMapper;
 
     @Override
-    public void onMessage(Message message, byte[] pattern) {
+    public void onMessage(@Nullable Message message, byte[] pattern) {
+        if (message == null) {
+            log.error("Received message is null in GoalCompletedEventListener");
+            throw new MessageProcessingException("Received message is null");
+        }
         try {
             GoalCompletedEvent goalCompletedEvent = objectMapper.readValue(message.getBody(), GoalCompletedEvent.class);
             AnalyticsEvent event = goalCompletedMapper.toEntity(goalCompletedEvent);
