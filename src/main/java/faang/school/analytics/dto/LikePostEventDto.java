2b4468b355baefd075be0fe0da6a677f0b54a526
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-12-15 17:51:21 +0500

    https://faang-school.atlassian.net/browse/BJS2-41456
    Поправил косяки

--- src/main/java/faang/school/analytics/dto/LikePostEventDto.java
+++ src/main/java/faang/school/analytics/dto/LikePostEventDto.java
@@ -1,13 +1,17 @@
 package faang.school.analytics.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LikePostEventDto {
 
     @NotNull
