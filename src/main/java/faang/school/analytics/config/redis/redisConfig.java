52528d36cbd3ebfc5537227811ce11f5450ed0ac
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-12-15 08:58:47 +0300

    BJS2-41408 анализ завершенных целей

--- src/main/java/faang/school/analytics/config/redis/redisConfig.java
+++ src/main/java/faang/school/analytics/config/redis/redisConfig.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -17,7 +16,7 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
-public class redisConfig {
+public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
 
