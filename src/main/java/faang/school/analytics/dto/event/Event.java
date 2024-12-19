50c145be14bbc3fcde89da387fcc550b6c7594fe
evgen-backend-rostov <googlbond1991@gmail.com>
2024-12-19 22:02:40 +0300

    BJS2-41347 реализовал слушатель событий для рекомендаций

---
+++ src/main/java/faang/school/analytics/dto/event/Event.java
@@ -0,0 +1,5 @@
+package faang.school.analytics.dto.event;
+
+public interface Event {
+
+}
--- src/main/java/faang/school/analytics/mapper/AnalyticsMapper.java
+++ src/main/java/faang/school/analytics/mapper/AnalyticsMapper.java
@@ -2,8 +2,10 @@ package faang.school.analytics.mapper;
 
 import faang.school.analytics.dto.AnalyticsCreateEventDto;
 import faang.school.analytics.dto.AnalyticsEventDto;
+import faang.school.analytics.dto.event.RecommendationEventDto;
 import faang.school.analytics.model.AnalyticsEvent;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -11,6 +13,8 @@ public interface AnalyticsMapper {
 
     AnalyticsEvent toEntity(AnalyticsEventDto analyticsEventDto);
     AnalyticsEvent toEntity(AnalyticsCreateEventDto analyticsCreateEventDto);
-
     AnalyticsEventDto toDto(AnalyticsEvent analyticsEvent);
+    @Mapping(source = "authorId", target = "actorId")
+    @Mapping(source = "createdAt", target = "receivedAt")
+    AnalyticsCreateEventDto fromEventToCreateDto(RecommendationEventDto recommendationEventDto);
 }
