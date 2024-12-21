4e8c2a29d7243c9ddf888c1dba6f22b794fef611
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-12-21 16:04:31 +0300

    BJS2-41408 анализ завершенных целей

--- src/main/java/faang/school/analytics/listener/FundRaisedEventListener.java
+++ src/main/java/faang/school/analytics/listener/FundRaisedEventListener.java
@@ -5,31 +5,31 @@ import faang.school.analytics.dto.FundRaisedEvent;
 import faang.school.analytics.dto.event.EventDto;
 import faang.school.analytics.mapper.fund_raised.FundRaisedEventMapper;
 import faang.school.analytics.service.event.AnalyticsEventService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
-@RequiredArgsConstructor
 @Slf4j
-public class FundRaisedEventListener implements MessageListener {
-    private final ObjectMapper objectMapper;
+public class FundRaisedEventListener extends AbstractListenerForAddEvent<FundRaisedEvent>{
     private final AnalyticsEventService analyticsEventService;
     private final FundRaisedEventMapper fundRaisedEventMapper;
 
+    public FundRaisedEventListener(ObjectMapper objectMapper,
+                                   AnalyticsEventService analyticsEventService,
+                                   FundRaisedEventMapper fundRaisedEventMapper) {
+        super(objectMapper);
+        this.analyticsEventService = analyticsEventService;
+        this.fundRaisedEventMapper = fundRaisedEventMapper;
+    }
+
     @Override
-    public void onMessage(Message message, byte[] pattern) {
-        try {
-            FundRaisedEvent fundRaisedEvent = objectMapper.readValue(message.getBody(), FundRaisedEvent.class);
-            EventDto eventDto = fundRaisedEventMapper.toEventDto(fundRaisedEvent);
-            analyticsEventService.addNewEvent(eventDto);
-        } catch (IOException e) {
-            log.error("Error processing FundRaisedEvent", e);
-            throw new RuntimeException(e);
-        }
+    public Class<FundRaisedEvent> getEventClass() {
+        return FundRaisedEvent.class;
+    }
+
+    @Override
+    public void handleEvent(FundRaisedEvent event) {
+        EventDto eventDto = fundRaisedEventMapper.toEventDto(event);
+        analyticsEventService.addNewEvent(eventDto);
     }
 }
