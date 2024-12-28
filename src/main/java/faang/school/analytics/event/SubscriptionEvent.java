9cf223c05de4bfee4779e3c88c1bb2458dd5616f
Dmitrii Pazinich <pazinich.d@yandex.ru>
2024-12-28 07:00:06 +0300

    Update code to address reviewer comments

--- src/main/java/faang/school/analytics/event/SubscriptionEvent.java
+++ src/main/java/faang/school/analytics/event/SubscriptionEvent.java
@@ -1,12 +1,13 @@
 package faang.school.analytics.event;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
-public record SubscriptionEvent(
-        Long followerId,
-        Long followeeId,
-        LocalDateTime subscribedAt,
-        String followerName,
-        String followeeName
-) {
+@Builder
+public record SubscriptionEvent(Long followerId,
+                                Long followeeId,
+                                LocalDateTime subscribedAt,
+                                String followerName,
+                                String followeeName) {
 }
--- src/test/java/faang/school/analytics/listener/SubscriptionEventListenerTest.java
+++ src/test/java/faang/school/analytics/listener/SubscriptionEventListenerTest.java
@@ -56,7 +56,7 @@ class SubscriptionEventListenerTest {
     @BeforeEach
     void setUp() {
         analyticsEvent = AnalyticsEvent.builder().id(100L).build();
-        subscriptionEvent = new SubscriptionEvent(1L, 2L, null, null, null);
+        subscriptionEvent = SubscriptionEvent.builder().followerId(1L).followeeId(2L).build();
         analyticsEventCaptor = ArgumentCaptor.forClass(AnalyticsEvent.class);
         json = "{\"followerId\":1,\"followeeId\":2,\"subscribedAt\":\"2024-12-10T12:00:00\"}";
         invalidJson = "{\"followerId\":\"invalid_id\",\"followeeId\":2,\"subscribedAt\":\"invalid_date\"}";
