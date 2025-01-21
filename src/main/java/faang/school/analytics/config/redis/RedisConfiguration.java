f1558561a05bd5d9ddea8cb2f3d81771ceb938dd
Stanislaav Kozh <printks@yandex.ru>
2025-01-21 19:05:29 +0300

    fix bag with merge

--- src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
@@ -1,5 +1,6 @@
 package faang.school.analytics.config.redis;
 
+import faang.school.analytics.listeners.CommentEventListener;
 import faang.school.analytics.listeners.RecommendationEventListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,9 +8,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,6 +24,8 @@ public class RedisConfiguration {
     private String host;
     @Value("${spring.data.redis.port}")
     private int port;
+    @Value("${spring.data.redis.channel.comment}")
+    private String commentChannelName;
 
 
     private final RecommendationEventListener recommendationEventListener;
@@ -32,13 +37,42 @@ public class RedisConfiguration {
     }
 
     @Bean
-    public RedisMessageListenerContainer redisContainer(JedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public MessageListenerAdapter commentListener(CommentEventListener commentEventListener) {
+        return new MessageListenerAdapter(commentEventListener);
+    }
+
+    @Bean
+    public ChannelTopic commentTopic() {
+        return new ChannelTopic(commentChannelName);
+    }
+
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer(JedisConnectionFactory connectionFactory, MessageListenerAdapter commentListener) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
 
         MessageListenerAdapter recommendationListenerAdapter = new MessageListenerAdapter(recommendationEventListener);
         container.addMessageListener(recommendationListenerAdapter, new ChannelTopic(userRecommendationChannel));
+        container.addMessageListener(commentListener, commentTopic());
 
         return container;
     }
+
+//    @Bean
+//    public RedisMessageListenerContainer redisContainer(MessageListenerAdapter commentListener) {
+//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+//        container.setConnectionFactory(connectionFactory());
+//        container.addMessageListener(commentListener, commentTopic());
+//        return container;
+//    }
 }
--- src/main/java/faang/school/analytics/listeners/CommentEventListener.java
+++ src/main/java/faang/school/analytics/listeners/CommentEventListener.java
@@ -1,11 +1,11 @@
-package faang.school.analytics.listener;
+package faang.school.analytics.listeners;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import faang.school.analytics.dto.AnalyticsEventMapper;
 import faang.school.analytics.dto.CommentEvent;
+import faang.school.analytics.mapper.AnalyticsMapper;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.model.EventType;
-import faang.school.analytics.service.AnalyticsEventService;
+import faang.school.analytics.service.AnalyticsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
@@ -19,17 +19,17 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class CommentEventListener implements MessageListener {
 
-    private final AnalyticsEventMapper analyticsEventMapper;
+    private final AnalyticsMapper analyticsMapper;
 
     private final ObjectMapper objectMapper;
-    private final AnalyticsEventService analyticsEventService;
+    private final AnalyticsService analyticsEventService;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
         CommentEvent event = null;
         try {
             event = objectMapper.readValue(message.getBody(), CommentEvent.class);
-            AnalyticsEvent mapperAnalyticsEvent = analyticsEventMapper.toAnalyticsEvent(event);
+            AnalyticsEvent mapperAnalyticsEvent = analyticsMapper.toAnalyticsEvent(event);
             mapperAnalyticsEvent.setEventType(EventType.POST_COMMENT);
             AnalyticsEvent analyticsEvent = analyticsEventService.addAnalyticsEvent(mapperAnalyticsEvent);
             log.info("CommentEventListener CommentEvent: {}", event);
