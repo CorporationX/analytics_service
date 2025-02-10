3cdb0826f68b4497c898ebeba40627e14b434ae9
oxana <nov2303@mail.ru>
2025-02-10 23:04:40 +0500

    BJS2-53233 | Сохранение и получение аналитики

--- src/main/java/faang/school/analytics/service/Interval.java
+++ src/main/java/faang/school/analytics/service/Interval.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 
 public enum Interval {
     ONE_MONTH(1),
-    THREE_MONTH(1),
+    THREE_MONTH(3),
     SIX_MONTH(6),
     ONE_YEAR(12);
     private final long offsetInMonths;
