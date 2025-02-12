0191e3d8c33e9585a334f013c03d48955aee677f
kaktus <dda20040609@gmail.com>
2025-02-12 15:30:17 +0300

    ci и контекст

---
+++ src/test/java/faang/school/analytics/AnalyticsServiceContextTest.java
@@ -0,0 +1,35 @@
+package faang.school.analytics;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ActiveProfiles("test")
+@SpringBootTest(classes = AnalyticsServiceApp.class)
+public class AnalyticsServiceContextTest {
+
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.2")
+            .withDatabaseName("test_db")
+            .withUsername("test_user")
+            .withPassword("test_password");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+
+    }
+}
