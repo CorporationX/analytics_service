0c3eafe59a793d852fd5a8b23b3afbfca2293079
OxanaNovozhilova <nov2303@mail.ru>
2025-02-22 01:10:40 +0500

    BJS2-53233 | Сохранение и получение аналитики (#384)
    * BJS2-53233 | Сохранение и получение аналитики
    
    * BJS2-53233 | Сохранение и получение аналитики
    
    * BJS2-53233 | Сохранение и получение аналитики
    
    * BJS2-53233 | Сохранение и получение аналитики
    
    * BJS2-53233 | Сохранение и получение аналитики
    
    * BJS2-53233 | Сохранение и получение аналитики
    
    * BJS2-53233 | Сохранение и получение аналитики
    
    * BJS2-53233 | Сохранение и получение аналитики
    
    * BJS2-53233 | Сохранение и получение аналитики

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
+    id("checkstyle")
 }
 
 group = "faang.school"
@@ -62,8 +63,31 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+checkstyle {
+    toolVersion = "10.17.0"
+    configFile = file("${project.rootDir}/config/checkstyle/checkstyle.xml")
+    checkstyle.enableExternalDtdLoad.set(true)
+}
+
+tasks.checkstyleMain {
+    source = fileTree("${project.rootDir}/src/main/java")
+    include("**/*.java")
+    exclude("**/resources/**")
+
+    classpath = files()
+}
+
+tasks.checkstyleTest {
+    source = fileTree("${project.rootDir}/src/test")
+    include("**/*.java")
+
+    classpath = files()
+}
+
 val test by tasks.getting(Test::class) { testLogging.showStandardStreams = true }
 
 tasks.bootJar {
     archiveFileName.set("service.jar")
 }
+
+
---
+++ src/test/java/faang/school/analytics/service/impl/AnalyticsEventServiceImplTest.java
@@ -0,0 +1,137 @@
+package faang.school.analytics.service.impl;
+
+import faang.school.analytics.dto.AnalyticsEventDto;
+import faang.school.analytics.dto.AnalyticsEventRequestDto;
+import faang.school.analytics.mapper.AnalyticsEventMapperImpl;
+import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.model.EventType;
+import faang.school.analytics.repository.AnalyticsEventRepository;
+import faang.school.analytics.service.Interval;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AnalyticsEventServiceImplTest {
+
+    @InjectMocks
+    private AnalyticsEventServiceImpl analyticsEventService;
+
+    @Mock
+    private AnalyticsEventRepository analyticsEventRepository;
+    @Spy
+    private AnalyticsEventMapperImpl analyticsEventMapper = new AnalyticsEventMapperImpl();
+
+
+    private static final long ACTOR_ID = 2L;
+    private static final long RECEIVER_ID = 3L;
+    private static final EventType EVENT_TYPE = EventType.POST_LIKE;
+
+
+    private AnalyticsEvent analyticsEvent = new AnalyticsEvent();
+    private AnalyticsEventDto analyticsEventDto = new AnalyticsEventDto();
+    private AnalyticsEvent analyticsEventFailed = new AnalyticsEvent();
+    private Stream<AnalyticsEvent> analyticsEventStream;
+    private Stream<AnalyticsEvent> analyticsEventStreamInterval;
+
+    @BeforeEach
+    void setUp() {
+        analyticsEventDto.setEventType(EVENT_TYPE);
+        analyticsEventDto.setActorId(ACTOR_ID);
+        analyticsEventDto.setReceivedAt(LocalDateTime.now());
+        analyticsEventDto.setReceiverId(RECEIVER_ID);
+
+        analyticsEventFailed.setEventType(EVENT_TYPE);
+
+        analyticsEventStream = prepareStreamForTest(false);
+        analyticsEventStreamInterval = prepareStreamForTest(true);
+
+        analyticsEventMapper = new AnalyticsEventMapperImpl();
+
+    }
+
+    private Stream<AnalyticsEvent> prepareStreamForTest(boolean interval) {
+        List<AnalyticsEvent> analyticsEventList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            LocalDateTime date = !interval ? LocalDateTime.now() : LocalDateTime.now().minusMonths(5);
+            analyticsEventList.add(new AnalyticsEvent(i, RECEIVER_ID, ACTOR_ID, EVENT_TYPE, date));
+        }
+        return analyticsEventList.stream();
+    }
+
+    @Test
+    public void testSaveEvent() {
+        analyticsEvent = analyticsEventMapper.toEntity(analyticsEventDto);
+        analyticsEventService.saveEvent(analyticsEventDto);
+        verify(analyticsEventRepository, times(1)).save(analyticsEvent);
+    }
+
+
+    @Test
+    public void testGetAnalyticsFromTo() {
+        AnalyticsEventRequestDto analyticsEventRequestDto = new AnalyticsEventRequestDto();
+        analyticsEventRequestDto.setReceiverId(RECEIVER_ID);
+        analyticsEventRequestDto.setEventType(EVENT_TYPE);
+        analyticsEventRequestDto.setFrom(LocalDateTime.now().minusDays(6));
+        analyticsEventRequestDto.setTo(LocalDateTime.now());
+
+        when(analyticsEventRepository.findByReceiverIdAndEventType(RECEIVER_ID, EVENT_TYPE))
+                .thenReturn(analyticsEventStream);
+        List<AnalyticsEventDto> list = analyticsEventService.getAnalytics(analyticsEventRequestDto);
+        Assert.assertEquals(10, list.size());
+    }
+
+    @Test
+    public void testGetAnalyticsFromToNoData() {
+        AnalyticsEventRequestDto analyticsEventRequestDto = new AnalyticsEventRequestDto();
+        analyticsEventRequestDto.setReceiverId(RECEIVER_ID);
+        analyticsEventRequestDto.setEventType(EVENT_TYPE);
+        analyticsEventRequestDto.setFrom(LocalDateTime.now().minusDays(6));
+        analyticsEventRequestDto.setTo(LocalDateTime.now().minusDays(3));
+
+        when(analyticsEventRepository.findByReceiverIdAndEventType(RECEIVER_ID, EVENT_TYPE))
+                .thenReturn(analyticsEventStream);
+        List<AnalyticsEventDto> list = analyticsEventService.getAnalytics(analyticsEventRequestDto);
+        Assert.assertEquals(0, list.size());
+    }
+
+    @Test
+    public void testGetAnalyticsIntervalFailed() {
+        AnalyticsEventRequestDto analyticsEventRequestDto = new AnalyticsEventRequestDto();
+        analyticsEventRequestDto.setReceiverId(RECEIVER_ID);
+        analyticsEventRequestDto.setEventType(EVENT_TYPE);
+        analyticsEventRequestDto.setInterval(Interval.ONE_MONTH);
+
+        when(analyticsEventRepository.findByReceiverIdAndEventType(RECEIVER_ID, EVENT_TYPE))
+                .thenReturn(analyticsEventStreamInterval);
+        List<AnalyticsEventDto> list = analyticsEventService.getAnalytics(analyticsEventRequestDto);
+        Assert.assertEquals(0, list.size());
+    }
+
+    @Test
+    public void testGetAnalyticsInterval() {
+        AnalyticsEventRequestDto analyticsEventRequestDto = new AnalyticsEventRequestDto();
+        analyticsEventRequestDto.setReceiverId(RECEIVER_ID);
+        analyticsEventRequestDto.setEventType(EVENT_TYPE);
+        analyticsEventRequestDto.setInterval(Interval.ONE_YEAR);
+
+        when(analyticsEventRepository.findByReceiverIdAndEventType(RECEIVER_ID, EVENT_TYPE))
+                .thenReturn(analyticsEventStreamInterval);
+        List<AnalyticsEventDto> list = analyticsEventService.getAnalytics(analyticsEventRequestDto);
+        Assert.assertEquals(10, list.size());
+    }
+}
