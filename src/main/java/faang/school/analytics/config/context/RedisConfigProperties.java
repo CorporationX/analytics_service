cda08125ea542f971eee47cde1d6c879beeb9e98
Mihail Ermolaev <fitis_rus21@mail.ru>
2025-02-24 19:28:25 +0300

    update properties

--- src/main/java/faang/school/analytics/config/context/RedisConfigProperties.java
+++ src/main/java/faang/school/analytics/config/context/RedisConfigProperties.java
@@ -2,9 +2,9 @@ package faang.school.analytics.config.context;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "spring.data.redis")
 @Data
 public class RedisConfigProperties {
