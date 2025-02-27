3702e487ced26c679303bba79c6ff6b83b77d46d
Ivan Baev <ibai86@yandex.ru>
2025-02-27 03:32:55 +0300

    refactor

--- src/main/java/faang/school/analytics/service/AnalyticsEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -5,15 +5,17 @@ import faang.school.analytics.repository.AnalyticsEventRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class AnalyticsEventService {
-    private final AnalyticsEventRepository repository;
+    private final AnalyticsEventRepository analyticsEventRepository;
 
+    @Transactional
     public void saveViewPostEvent(AnalyticsEvent event) {
-        repository.save(event);
-        log.info("Event #Post view# with id {} save to repository", event.getId());
+        analyticsEventRepository.save(event);
+        log.info("Event #Post view# with id {} successfully saved", event.getId());
     }
 }
