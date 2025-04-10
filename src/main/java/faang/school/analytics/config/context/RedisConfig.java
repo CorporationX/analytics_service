884195eb054534a043b79511a6fd8f160de85c02
Ilya Yanchenko <zsdfghjkl111@gmail.com>
2025-04-11 02:36:13 +0700

    add Listener

---
+++ src/main/java/faang/school/analytics/config/context/RedisConfig.java
@@ -0,0 +1,65 @@
+package faang.school.analytics.config.context;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import faang.school.analytics.listener.MentorShipRequestListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.channel.mentorship-request}")
+    private String mentorshipRequest;
+    private MentorShipRequestListener mentorShipRequestListener;
+
+    @Bean
+    JedisConnectionFactory JedisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfiguration =
+                new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(redisConfiguration);
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(JedisConnectionFactory());
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        return template;
+    }
+
+    @Bean
+    ChannelTopic MentorShipRequestTopic() {
+        return new ChannelTopic(mentorshipRequest);
+    }
+
+    @Bean
+    MessageListenerAdapter mentorShipListener() {
+        return new MessageListenerAdapter(mentorShipRequestListener);
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer(
+            MessageListenerAdapter mentorShipRequestListener) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(JedisConnectionFactory());
+        container.addMessageListener(mentorShipRequestListener, MentorShipRequestTopic());
+        return container;
+    }
+    
+}
