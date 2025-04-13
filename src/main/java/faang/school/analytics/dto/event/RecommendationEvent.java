ecf641d20a341de286126a73e8e14f219c141e8a
Marsel <mars1212@inbox.ru>
2025-04-13 18:28:55 +0300

    BJS2-66362  added RecommendationEventListener & RecommendationEvent

--- src/main/java/faang/school/analytics/dto/event/RecommendationEvent.java
+++ src/main/java/faang/school/analytics/dto/event/RecommendationEvent.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class RecommendationEvent {
-    private Long recommendationId;
     private Long authorId;
-    private Long recipientId;
+    private Long receiverId;
 }
--- src/main/java/faang/school/analytics/listener/RecommendationEventListener.java
+++ src/main/java/faang/school/analytics/listener/RecommendationEventListener.java
@@ -1,18 +1,25 @@
 package faang.school.analytics.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.analytics.config.redis.RedisListener;
 import faang.school.analytics.dto.event.RecommendationEvent;
-import faang.school.analytics.model.AnalyticsEvent;
+import faang.school.analytics.model.EventType;
 import faang.school.analytics.service.AnalyticsEventService;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
-@RequiredArgsConstructor
-public class RecommendationEventListener {
-    private final AnalyticsEventService analyticsEventService;
+@RedisListener(topic = "recommendationEvent")
+public class RecommendationEventListener extends AbstractEventListener<RecommendationEvent>{
 
+    public RecommendationEventListener(ObjectMapper objectMapper,
+                                       AnalyticsEventService analyticsEventService) {
+        super(objectMapper, analyticsEventService, RecommendationEvent.class);
+    }
 
-    public void handleMessage(RecommendationEvent event) {
-        AnalyticsEvent analyticsEvent = new AnalyticsEvent();
+    @Override
+    protected void handleEvent(RecommendationEvent event) {
+        saveAnalyticsEvent(event.getAuthorId(), event.getReceiverId(), EventType.RECOMMENDATION_RECEIVED);
     }
 }
