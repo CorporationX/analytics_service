9bfbfbd657f21fe4044e2e179f635432d05f3103
saroyangor <dr.saroyan98@gmail.com>
2025-04-14 20:55:38 +0300

    BJS2-65525:fix/add NoArgsConstructor

--- src/main/java/faang/school/analytics/dto/ProjectViewEvent.java
+++ src/main/java/faang/school/analytics/dto/ProjectViewEvent.java
@@ -2,11 +2,13 @@ package faang.school.analytics.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProjectViewEvent {
     private long projectId;
     private long userId;
