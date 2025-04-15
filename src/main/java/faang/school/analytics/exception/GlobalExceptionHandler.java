61fb75071718c71837d25e5aee3eefb74206c238
Max Goloviznin <roxet59rus@mail.ru>
2025-04-15 18:30:09 +0500

    (BJS2-65531) fix by comments

--- src/main/java/faang/school/analytics/exception/GlobalExceptionHandler.java
+++ src/main/java/faang/school/analytics/exception/GlobalExceptionHandler.java
@@ -3,9 +3,8 @@ package faang.school.analytics.exception;
 import faang.school.analytics.dto.error.ErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -18,25 +17,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({
             EntityNotFoundException.class
     })
-    @ResponseStatus(NOT_FOUND)
-    public ErrorResponse handleExceptionsWithStatusNotFound(Exception ex) {
-        return getErrorResponse(ex, NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleExceptionsWithStatusNotFound(Exception ex) {
+        return ResponseEntity.status(NOT_FOUND).body(getErrorResponse(ex));
     }
 
     @ExceptionHandler({
             AnalyticsConvertingException.class
     })
-    @ResponseStatus(BAD_REQUEST)
-    public ErrorResponse handleExceptionsWithStatusBadRequest(Exception ex) {
-        return getErrorResponse(ex, BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleExceptionsWithStatusBadRequest(Exception ex) {
+        return ResponseEntity.status(BAD_REQUEST).body(getErrorResponse(ex));
     }
 
-    private ErrorResponse getErrorResponse(Exception ex, HttpStatus status) {
+    private ErrorResponse getErrorResponse(Exception ex) {
         log.error("{}", ex.toString());
         return ErrorResponse.builder()
                 .message(ex.getMessage())
-                .statusCode(status.value())
-                .statusName(status.name())
                 .build();
     }
 }
