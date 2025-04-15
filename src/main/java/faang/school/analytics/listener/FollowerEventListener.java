923449116e16eaa3fa8d25fdc9ac616413153f6e
Vitaliy <vitaliy.zhilitskiy@gmail.com>
2025-04-15 16:57:23 +0300

    Задача BJS2-66366 выполнена: - добавлена возможность сбора аналитики подписок пользователя

---
+++ src/main/java/faang/school/analytics/listener/FollowerEventListener.java
@@ -0,0 +1,30 @@
+package faang.school.analytics.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.analytics.config.redis.RedisListener;
+import faang.school.analytics.dto.subscription.FollowerEventDto;
+import faang.school.analytics.model.EventType;
+import faang.school.analytics.service.AnalyticsEventService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Слушатель событий подписки пользователей.
+ * <p>
+ * Получает события о подписках пользователей из Redis и сохраняет их в базе данных.
+ * </p>
+ */
+@Slf4j
+@Component
+@RedisListener(topic = "follower_event")
+public class FollowerEventListener extends AbstractEventListener<FollowerEventDto> {
+    public FollowerEventListener(ObjectMapper objectMapper, AnalyticsEventService analyticsEventService) {
+        super(objectMapper, analyticsEventService, FollowerEventDto.class);
+    }
+
+    @Override
+    protected void handleEvent(FollowerEventDto event) {
+        log.info("Received follower event: {}", event);
+        saveAnalyticsEvent(event.getFollowerId(), event.getFolloweeId(), EventType.FOLLOWER);
+    }
+}
