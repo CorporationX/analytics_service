8cf7f28ac4e5ee09e59f15a332892104c01e76cc
Vladimir Levshin <levshin@intervolga.ru>
2025-06-09 21:57:34 +0300

    Added redis pub/sub config.

---
+++ src/main/java/faang/school/analytics/config/redis/RedisProperties.java
@@ -0,0 +1,16 @@
+package faang.school.analytics.config.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+@ConfigurationProperties("spring.data.redis")
+public record RedisProperties(
+        String host,
+        int port,
+        @NestedConfigurationProperty ChannelNames channelNames
+) {
+    public record ChannelNames(
+            String profileView
+    ) {
+    }
+}
