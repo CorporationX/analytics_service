fb25215e94d9a94c1c002c05a94dcb75933df14e
v888.rus <v888.rus@gmail.com>
2025-06-11 14:34:39 +0000

    Fix(1). 77891 Save and get analytics. Team's review.

--- src/main/java/faang/school/analytics/dto/AnalyticsEventRequestDto.java
+++ src/main/java/faang/school/analytics/dto/AnalyticsEventRequestDto.java
@@ -11,6 +11,6 @@ public class AnalyticsEventRequestDto {
     private long receiverId; 
     private String eventType;
     private String interval;
-    private String from; // e.g., "2025-06-10T14:30:00"
-    private String to;   // e.g., "2025-06-10T14:30:00"
+    private String from;
+    private String to;
 }
