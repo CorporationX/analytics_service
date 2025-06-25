fb404617bf4168292e7fe14eae2d93f15db71773
Vladislav Kozlov <littlerock2877@mail.ru>
2025-06-25 21:04:18 +0300

    fix: pr comments

--- src/main/java/faang/school/analytics/dto/premium/PremiumBoughtEvent.java
+++ src/main/java/faang/school/analytics/dto/premium/PremiumBoughtEvent.java
@@ -3,5 +3,6 @@ package faang.school.analytics.dto.premium;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record PremiumBoughtEvent(Long userId, BigDecimal price, Currency currency, PremiumPeriod premiumPeriod, LocalDateTime startDate) {
+public record PremiumBoughtEvent(Long userId, BigDecimal price, Currency currency, PremiumPeriod premiumPeriod,
+                                 LocalDateTime startDate) {
 }
