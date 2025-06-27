107f6fa085b083e6e06ee4c7489c069e6d76b4cb
firma78 <rap215@icloud.com>
2025-06-27 05:20:46 +0300

    Without Tests

--- src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfiguration.java
@@ -8,13 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
