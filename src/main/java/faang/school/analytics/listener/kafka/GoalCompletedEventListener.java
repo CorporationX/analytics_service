af20c36419f5fb8784673b609f8f4e09beb848d9
Aleksandr <alexandr.sidorkin@gmail.com>
2025-07-04 22:31:47 +0200

    BJS2-83562 refactoring after review
    Properties for kafka config

--- src/main/java/faang/school/analytics/listener/kafka/GoalCompletedEventListener.java
+++ src/main/java/faang/school/analytics/listener/kafka/GoalCompletedEventListener.java
@@ -2,12 +2,12 @@ package faang.school.analytics.listener.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.analytics.config.kafka.KafkaProperties;
 import faang.school.analytics.dto.GoalCompletedEvent;
 import faang.school.analytics.mapper.event.UserServiceEventMapper;
 import faang.school.analytics.service.AnalyticsEventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -18,13 +18,11 @@ public class GoalCompletedEventListener {
     private final ObjectMapper objectMapper;
     private final AnalyticsEventService service;
     private final UserServiceEventMapper userServiceEventMapper;
+    private final KafkaProperties properties;
 
-    @Value("${spring.data.kafka.use-kafka}")
-    private boolean useKafka;
-
-    @KafkaListener(topics = "goal_completed")
+    @KafkaListener(topics = "${spring.data.kafka.topics.goal-completed}")
     public void listen(String json) {
-        if (!useKafka) return;
+        if (!properties.isUseKafka()) return;
         try {
             GoalCompletedEvent event = objectMapper.readValue(json, GoalCompletedEvent.class);
             service.saveEvent(userServiceEventMapper.goalCompleteToAnalytics(event));
