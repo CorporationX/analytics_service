2c06a83a932237544f02b1d3392a02711b771ae4
firma78 <rap215@icloud.com>
2025-07-23 03:25:20 +0300

    fix

--- src/main/java/faang/school/analytics/model/FollowerEvent.java
+++ src/main/java/faang/school/analytics/model/FollowerEvent.java
@@ -4,19 +4,17 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-    @Data
-    @Builder
-    @Component
-    @RequiredArgsConstructor
-    public class FollowerEvent {
+@Data
+@Builder
+@RequiredArgsConstructor
+public class FollowerEvent {
 
-        @NotNull
-        private final Long followerId;
-        private final Long followeeId;
-        private final Long projectId;
-        private final LocalDateTime subscriptionTime;
-    }
+    @NotNull
+    private final Long followerId;
+    private final Long followeeId;
+    private final Long projectId;
+    private final LocalDateTime subscriptionTime;
+}
