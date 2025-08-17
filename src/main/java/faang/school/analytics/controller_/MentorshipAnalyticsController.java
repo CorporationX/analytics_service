6c69cee8354138b41c22fbff229b001897b9a3ed
Yulia <bualex1983@mail.ru>
2025-08-17 17:36:24 +0300

    addition

---
+++ src/main/java/faang/school/analytics/controller_/MentorshipAnalyticsController.java
@@ -0,0 +1,33 @@
+package faang.school.analytics.controller_;
+
+import faang.school.analytics.dto.MentorshipEventDto;
+import faang.school.analytics.service.MentorshipEventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/mentorship")
+@RequiredArgsConstructor
+public class MentorshipAnalyticsController {
+
+    private final MentorshipEventService mentorshipEventService;
+
+    @GetMapping("/mentorshiprequest/{userId}")
+    public List<MentorshipEventDto> getUserMentorshipAnalytics(
+            @PathVariable long userId,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to) {
+
+        return mentorshipEventService.getUserMentorshipAnalytics(userId, from, to);
+    }
+}
