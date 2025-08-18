e413e0b9b8d903eff0e6c18a4173fee9fbbf8ed2
Ivi-Anastasia <qwwsoul@gmail.com>
2025-08-18 13:37:34 +0100

    Addressed feedback: updated mapping for Redis task

--- src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
@@ -1,10 +1,13 @@
 package faang.school.analytics.mapper;
 
 import faang.school.analytics.dto.AnalyticsEventDto;
+import faang.school.analytics.dto.MentorshipRequestedEvent;
 import faang.school.analytics.model.AnalyticsEvent;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
 public interface AnalyticsEventMapper {
     AnalyticsEventDto toAnalyticsEventDto(AnalyticsEvent analyticsEvent);
+
+    AnalyticsEvent toAnalyticsEvent(MentorshipRequestedEvent mentorshipRequestedEvent);
 }
