736573f19edd51a37edce6326b2cd7073b58696c
Ivi-Anastasia <qwwsoul@gmail.com>
2025-08-18 21:57:37 +0100

    Corrected mapping logic

--- src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
+++ src/main/java/faang/school/analytics/mapper/AnalyticsEventMapper.java
@@ -4,10 +4,12 @@ import faang.school.analytics.dto.AnalyticsEventDto;
 import faang.school.analytics.dto.MentorshipRequestedEvent;
 import faang.school.analytics.model.AnalyticsEvent;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
 public interface AnalyticsEventMapper {
     AnalyticsEventDto toAnalyticsEventDto(AnalyticsEvent analyticsEvent);
 
+    @Mapping(target = "eventType", constant = "MENTORSHIP_REQUESTED")
     AnalyticsEvent toAnalyticsEvent(MentorshipRequestedEvent mentorshipRequestedEvent);
 }
