6e7b3c634ec6f55d55c53a2527c084e7450abea7
Tien-PhanT <112714509+phtanya@users.noreply.github.com>
2025-08-31 18:25:54 +0200

    BJS2-83806

--- src/main/java/faang/school/analytics/config/redis/RedisConfig.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfig.java
@@ -18,13 +18,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @RequiredArgsConstructor
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    @Value("${spring.data.redis.channel.goal_completed}")
+    @Value("${spring.data.redis.channel.goal-completed}")
     private String goalCompletedChannel;
 
     @Bean
