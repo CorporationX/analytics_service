bd01896763de5f2fb2a971050eb1778749256115
Elya Khamzina <ekkhamzi@mail.ru>
2025-11-13 23:39:54 +0300

    feature-BJS2-96865-comment-analysis-fix

--- src/main/java/faang/school/analytics/service/AnalyticsEventService.java
+++ src/main/java/faang/school/analytics/service/AnalyticsEventService.java
@@ -1,12 +1,9 @@
 package faang.school.analytics.service;
 
 import faang.school.analytics.dto.AnalyticsEventResponseDto;
-import faang.school.analytics.dto.CommentEventDto;
-import faang.school.analytics.dto.EventDto;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.model.EventType;
 import faang.school.analytics.model.Interval;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -25,13 +22,14 @@ public interface AnalyticsEventService {
     void saveEvent(AnalyticsEvent event);
 
     /**
-     * Сохраняет аналитическое событие в базу данных из DTO.
-     * Преобразует EventDto в AnalyticsEvent с помощью маппера.
+     * Сохраняет аналитическое событие в базу данных из DTO объекта.
+     * Автоматически определяет тип события и преобразует в сущность через маппер.
      *
-     * @param eventDto DTO объект аналитического события для сохранения
+     * @param eventDto DTO объект события для сохранения (EventDto, CommentEventDto, etc.)
      * @throws AnalyticsValidationException если данные события не прошли валидацию
+     * @throws IllegalArgumentException если передан неподдерживаемый тип DTO
      */
-    void saveEvent(EventDto eventDto);
+    void saveEvent(Object eventDto);
 
     /**
      * Получает аналитику по событиям для указанного пользователя и типа события.
@@ -47,13 +45,4 @@ public interface AnalyticsEventService {
      * @throws IllegalArgumentException если не указан ни interval, ни from/to
      */
     List<AnalyticsEventResponseDto> getAnalytics(long receiverId, EventType eventType, Interval interval, LocalDateTime from, LocalDateTime to);
-
-    /**
-     * Сохраняет аналитическое событие в базу данных из DTO.
-     * Преобразует CommentEventDto в AnalyticsEvent с помощью маппера.
-     *
-     * @param commentEventDto DTO объект аналитического события для сохранения
-     * @throws AnalyticsValidationException если данные события не прошли валидацию
-     */
-    void saveEvent(CommentEventDto commentEventDto);
 }
