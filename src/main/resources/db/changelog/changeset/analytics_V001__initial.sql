db12db6e6d4501bfb7fe5e1ccf8fbc45a94fc154
Elya Khamzina <ekkhamzi@mail.ru>
2025-11-16 19:43:31 +0300

    feature-BJS2-96865-comment-analysis-fix

--- src/main/resources/db/changelog/changeset/analytics_V001__initial.sql
+++ src/main/resources/db/changelog/changeset/analytics_V001__initial.sql
@@ -3,7 +3,7 @@ CREATE TABLE analytics_event (
     receiver_id bigint NOT NULL,
     actor_id bigint NOT NULL,
     event_type varchar(64) NOT NULL,
-    received_at timestamptz DEFAULT current_timestamp,
+    received_at timestamptz DEFAULT current_timestamp
 );
 
 CREATE INDEX events_idx ON analytics_event(receiver_id, event_type, received_at DESC);
