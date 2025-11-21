ad358982f7975c2a9e2b1f8e95880be87c5182e4
Deathroels <loocyep@gmail.com>
2025-11-21 13:12:03 +0300

    -add acknowledgment ack for PostViewEventListener

--- src/main/java/faang/school/analytics/listener/PostViewEventListener.java
+++ src/main/java/faang/school/analytics/listener/PostViewEventListener.java
@@ -1,20 +1,14 @@
 package faang.school.analytics.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import faang.school.analytics.dto.PostViewEvent;
 import faang.school.analytics.service.AnalyticsEventService;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Component
 @RequiredArgsConstructor
@@ -28,13 +22,16 @@ public class PostViewEventListener {
             containerFactory = "postViewConcurrentKafkaListenerContainerFactory"
     )
 
-    public void consumePostViewEvent(ConsumerRecord<String, String> record) {
+    public void consumePostViewEvent(ConsumerRecord<String, String> record, Acknowledgment ack) {
         log.info("Received PostViewEvent from Kafka: {}", record.value());
 
         try {
             PostViewEvent event = objectMapper.readValue(record.value(), PostViewEvent.class);
             analyticsEventService.processPostViewEvent(event);
             log.info("Successfully processed PostViewEvent for postId: {}", event.postId());
+
+            ack.acknowledge();
+            log.info("Acknowledged message for postId: {}", event.postId());
         } catch (Exception e) {
             log.error("Error processing PostViewEvent: {}", e.getMessage());
         }
