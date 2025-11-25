0cac61d30eeec7909d67129a75a94706a3737cc8
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-11-25 15:32:36 +0100

    pipeline files

--- src/main/java/faang/school/analytics/config/context/UserHeaderFilter.java
+++ src/main/java/faang/school/analytics/config/context/UserHeaderFilter.java
@@ -25,7 +25,9 @@ public class UserHeaderFilter implements Filter {
         if (userId != null) {
             userContext.setUserId(Long.parseLong(userId));
         } else {
-            throw new IllegalArgumentException("Missing required header 'x-user-id'. Please include 'x-user-id' header with a valid user ID in your request.");
+            throw new IllegalArgumentException(
+                    "Missing required header 'x-user-id'. Include it with a valid user ID."
+            );
         }
         try {
             chain.doFilter(request, response);
