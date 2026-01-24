d756ec6dd9026576a89661e3d7821fdcd056c989
Ruslan <ruslan8020110@gmail.com>
2026-01-24 03:14:45 +0300

    исправления

--- src/main/java/faang/school/analytics/listener/FollowerEventListener.java
+++ src/main/java/faang/school/analytics/listener/FollowerEventListener.java
@@ -1,6 +1,7 @@
 package faang.school.analytics.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.analytics.client.UserServiceClient;
 import faang.school.analytics.dto.FollowerEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
